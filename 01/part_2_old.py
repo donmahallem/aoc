18e36bd69cf01e123fd9d1d277b631d655b9153a
donmahallem <donmahallem@users.noreply.github.com>
2024-12-02 00:50:52 +0100

    day1: castings

--- 01/part_2_old.py
+++ 01/part_2_old.py
@@ -11,6 +11,8 @@ with codecs.open("data_1.txt",encoding="utf8") as f:
         l1.append(int(a[0]))
         l2.append(int(a[1]))
 
-occurences=dict(map(lambda item1: (item1,len([ 1 for item2 in l2 if item2==item1])), set(l2)))
-output=sum(map(lambda item:( occurences[item]*item if item in occurences else 0),l1))
+output=0
+for item1 in l1:
+    similar = [ 1 for item2 in l2 if item2==item1]
+    output +=item1*len(similar)
 print(output)

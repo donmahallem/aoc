24b26505c2bcd97b868c96c4d26ea781d633024e
donmahallem <donmahallem@users.noreply.github.com>
2024-12-02 20:50:22 +0100

    5 mal falsch abgebogen

---
+++ 02/part_1.py
@@ -0,0 +1,28 @@
+import codecs
+
+with codecs.open("data_1.txt", encoding="utf8") as f:
+    data = f.readlines()
+
+def checkRow(row):
+    inc=None
+    for i in range(0,len(row)-1):
+        if(row[i]==row[i+1]):
+            return False
+        if abs(row[i]-row[i+1])>3:
+            return False
+        if not inc is None:
+            if(row[i]<row[i+1] and not inc):
+                return False
+            if(row[i]>row[i+1] and inc):
+                return False
+        else:
+            inc = row[i]<row[i+1]
+        
+    return True
+        
+sum = 0
+for dataline in data:
+    dataline2=[int(d) for d in dataline.split()]
+    if checkRow(dataline2):
+        sum+=1
+print(sum)
---
+++ 02/part_2.py
@@ -0,0 +1,33 @@
+import codecs
+
+with codecs.open("data_1.txt", encoding="utf8") as f:
+    data = f.readlines()
+
+def checkRow(row):
+    inc=None
+    for i in range(0,len(row)-1):
+        if(row[i]==row[i+1]):
+            return False
+        if abs(row[i]-row[i+1])>3:
+            return False
+        if not inc is None:
+            if(row[i]<row[i+1] and not inc):
+                return False
+            if(row[i]>row[i+1] and inc):
+                return False
+        else:
+            inc = row[i]<row[i+1]
+        
+    return True
+        
+sum = 0
+for dataline in data:
+    dataline2=[int(d) for d in dataline.split()]
+    if not checkRow(dataline2):
+        for i in range(0,len(dataline2)):
+            if(checkRow(dataline2[0:i]+dataline2[i+1:])):
+                sum+=1
+                break
+    else:
+        sum+=1
+print(sum)

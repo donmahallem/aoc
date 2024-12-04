9b6ddb8c0bca4f938b6ab2df68acdf8ae2bbdad8
donmahallem <4698322+donmahallem@users.noreply.github.com>
2024-12-04 07:51:14 +0000

    OMG, have you seen those snek pics? 🐍✨ Like, for real, these slithery bois are serving looks! 💅 1. This one’s got those iridescent scales that shine like a rainbow in the sun. 🌈✨ Legit, it’s like Mother Nature decided to flex on us. #SnekGoals 2. Spotted pattern? Check. Regal AF? Double check. This snek is basically the royalty of the reptile world. 👑🐍 Bow down, peasants. 3. Snek #3: Albino snek with those red eyes? Bruh, it’s like a mythical creature straight outta a fantasy novel. 🧙‍♂️✨ #SnekMagic 4. Green tree python just chilling on a branch, looking like it owns the place. 🌿🐍 This snek is the definition of “I woke up like this.” #Flawless
    And let’s not forget their sassy little tongues flicking out like they’re tasting the air for drama. 👅💅 These sneks are out here living their best lives, making us all look basic in comparison.
    
    So, next time you see a snek, remember: they’re not just reptiles, they’re fashion icons. 🐍✨ #SnekLife #SlitherAndShine
    
    Stay fabulous, sneks. We stan. 💖🐍

--- 04/part_1.py
+++ 04/part_1.py
@@ -1,40 +1,43 @@
 import codecs
 import regex
 import numpy as np
+
 with codecs.open("data.txt", encoding="utf8") as f:
     data = [a.strip() for a in f.readlines()]
-search_terms="XMAS"
-zeilen=len(data)
-spalten=len(data[0])
-count=0
+search_terms = "XMAS"
+zeilen = len(data)
+spalten = len(data[0])
+count = 0
 
-def checkDir(x,y,dirx,diry,term):
-    if(x+(dirx*len(term))+1<0 or x+(dirx*len(term))>spalten):
+
+def checkDir(x, y, dirx, diry, term):
+    if x + (dirx * len(term)) + 1 < 0 or x + (dirx * len(term)) > spalten:
         return False
-    elif(y+(diry*len(term))+1<0 or y+(diry*len(term))>zeilen):
+    elif y + (diry * len(term)) + 1 < 0 or y + (diry * len(term)) > zeilen:
         return False
     for i in range(len(term)):
-        if data[y+diry*i][x+dirx*i]!=term[i]:
+        if data[y + diry * i][x + dirx * i] != term[i]:
             return False
     return True
 
-count=0
-for y in range(0,zeilen):
+
+count = 0
+for y in range(0, zeilen):
     for x in range(spalten):
-        if checkDir(x,y,1,0,search_terms):
-            count+=1
-        if checkDir(x,y,1,-1,search_terms):
-            count+=1
-        if checkDir(x,y,0,-1,search_terms):
-            count+=1
-        if checkDir(x,y,-1,-1,search_terms):
-            count+=1
-        if checkDir(x,y,-1,0,search_terms):
-            count+=1
-        if checkDir(x,y,-1,1,search_terms):
-            count+=1
-        if checkDir(x,y,0,1,search_terms):
-            count+=1
-        if checkDir(x,y,1,1,search_terms):
-            count+=1
+        if checkDir(x, y, 1, 0, search_terms):
+            count += 1
+        if checkDir(x, y, 1, -1, search_terms):
+            count += 1
+        if checkDir(x, y, 0, -1, search_terms):
+            count += 1
+        if checkDir(x, y, -1, -1, search_terms):
+            count += 1
+        if checkDir(x, y, -1, 0, search_terms):
+            count += 1
+        if checkDir(x, y, -1, 1, search_terms):
+            count += 1
+        if checkDir(x, y, 0, 1, search_terms):
+            count += 1
+        if checkDir(x, y, 1, 1, search_terms):
+            count += 1
 print(count)
--- 04/part_2.py
+++ 04/part_2.py
@@ -1,36 +1,40 @@
 import codecs
 import regex
 import numpy as np
+
 with codecs.open("data.txt", encoding="utf8") as f:
     data = [a.strip() for a in f.readlines()]
-search_terms="XMAS"
-zeilen=len(data)
-spalten=len(data[0])
-def checkDir(x,y,dirx,diry,term):
-    if(x+(dirx*len(term))+1<0 or x+(dirx*len(term))>spalten):
+search_terms = "XMAS"
+zeilen = len(data)
+spalten = len(data[0])
+
+
+def checkDir(x, y, dirx, diry, term):
+    if x + (dirx * len(term)) + 1 < 0 or x + (dirx * len(term)) > spalten:
         return False
-    elif(y+(diry*len(term))+1<0 or y+(diry*len(term))>zeilen):
+    elif y + (diry * len(term)) + 1 < 0 or y + (diry * len(term)) > zeilen:
         return False
     for i in range(len(term)):
-        if x==3 and y==9 and dirx==-1:
-            print(data[y+diry*i][x+dirx*i],term[i])
-        if data[y+diry*i][x+dirx*i]!=term[i]:
+        if x == 3 and y == 9 and dirx == -1:
+            print(data[y + diry * i][x + dirx * i], term[i])
+        if data[y + diry * i][x + dirx * i] != term[i]:
             return False
     return True
 
-count=0
-for y in range(1,zeilen-1):
-    for x in range(1,spalten-1):
-        da=0
-        if data[y][x]=="A":
-            if data[y-1][x-1]=="M" and data[y+1][x+1]=="S":
-                da+=1
-            if data[y-1][x-1]=="S" and data[y+1][x+1]=="M":
-                da+=1
-            if data[y+1][x-1]=="M" and data[y-1][x+1]=="S":
-                da+=1
-            if data[y+1][x-1]=="S" and data[y-1][x+1]=="M":
-                da+=1
-        if da==2:
-            count+=1
+
+count = 0
+for y in range(1, zeilen - 1):
+    for x in range(1, spalten - 1):
+        da = 0
+        if data[y][x] == "A":
+            if data[y - 1][x - 1] == "M" and data[y + 1][x + 1] == "S":
+                da += 1
+            if data[y - 1][x - 1] == "S" and data[y + 1][x + 1] == "M":
+                da += 1
+            if data[y + 1][x - 1] == "M" and data[y - 1][x + 1] == "S":
+                da += 1
+            if data[y + 1][x - 1] == "S" and data[y - 1][x + 1] == "M":
+                da += 1
+        if da == 2:
+            count += 1
 print(count)

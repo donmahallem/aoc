e291a3296867864a9de32ed613b875ce2bb8bc1a
donmahallem <donmahallem@users.noreply.github.com>
2024-12-07 07:05:45 +0100

    add day07

---
+++ 07/part_2.py
@@ -0,0 +1,26 @@
+import codecs
+
+with codecs.open("data.txt", encoding="utf8") as f:
+    data = [a.strip() for a in f.readlines()]
+
+data = [row.split(":") for row in data]
+data = [
+    (int(result[0].strip()), [int(a) for a in result[1].strip().split(" ")])
+    for result in data
+]
+
+
+def isValidRow(row):
+    def nextOp(value, target, remaining_terms):
+        if len(remaining_terms) == 0:
+            return target == value
+        elif value > target:
+            return False
+        return (
+            nextOp(value + remaining_terms[0], target, remaining_terms[1:])
+            or nextOp(value * remaining_terms[0], target, remaining_terms[1:])
+            or nextOp(int(f"{value}{remaining_terms[0]}"), target, remaining_terms[1:])
+        )
+
+    result, terms = row
+    return nextOp(terms[0], result, terms[1:])

788b3244b078991978492db89cea53c5cef482cd
ò_ó <donmahallem@users.noreply.github.com>
2025-03-22 12:40:01 +0100

    add day09 (#21)

---
+++ go/aoc24/day09/part_2.go
@@ -0,0 +1,77 @@
+package day09
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func FindEmptySpace(inp *[]int16, size *int, rangeEnd *int) (int, int, bool) {
+	var startIdx, endIdx int
+	inside := false
+	for i := range *rangeEnd {
+		if !inside && (*inp)[i] == -1 {
+			startIdx = i
+			endIdx = startIdx + 1
+			inside = true
+		} else if inside {
+			if (*inp)[i] == -1 {
+				endIdx = i + 1
+			} else {
+				inside = false
+			}
+		}
+		if inside && endIdx-startIdx == *size {
+			return startIdx, endIdx, true
+		}
+
+	}
+	return -1, -1, false
+}
+
+func FindBlock(inp *[]int16, id *int16, maxIdx *int) (int, int, bool) {
+	var startIdx, endIdx int
+	inside := false
+	searchStart := *maxIdx
+	if searchStart < 0 {
+		searchStart = len((*inp)) - 1
+	}
+	for i := searchStart; i >= 0; i-- {
+		if !inside && (*inp)[i] == *id {
+			startIdx = i
+			endIdx = startIdx + 1
+			inside = true
+		} else if inside {
+			if (*inp)[i] == *id {
+				startIdx--
+			} else {
+				return startIdx, endIdx, true
+			}
+		}
+	}
+	return -1, -1, false
+}
+
+func CompactLess(inp *[]int16) {
+	blockId := (*inp)[len((*inp))-1]
+	var lastBlockStart int = -1
+	for ; blockId >= 0; blockId-- {
+		if blockStart, blockEnd, blockOk := FindBlock(inp, &blockId, &lastBlockStart); blockOk {
+			blockSize := blockEnd - blockStart
+			if spaceStart, _, spaceOk := FindEmptySpace(inp, &blockSize, &blockStart); spaceOk {
+				for i := 0; i < blockSize; i++ {
+					(*inp)[spaceStart+i] = (*inp)[blockStart+i]
+					(*inp)[blockStart+i] = -1
+				}
+			}
+			lastBlockStart = blockStart
+		}
+	}
+}
+
+func Part2() {
+	data, _ := io.ReadAll(os.Stdin)
+	expandedData := ConvertInput(&data)
+	CompactLess(&expandedData)
+	fmt.Printf("%d\n", CheckSum(&expandedData))
+}
---
+++ go/aoc24/day09/part_2_test.go
@@ -0,0 +1,56 @@
+package day09_test
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/donmahallem/aoc/aoc24/day09"
+)
+
+func TestFindEmptySpace(t *testing.T) {
+	test := []int16{0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2}
+	var lookFor int = 1
+	var endIdx int = 10
+	if start, end, ok := day09.FindEmptySpace(&test, &lookFor, &endIdx); start != 1 && end != 3 && !ok {
+		t.Errorf(`Expected (%d,%d,%t) and got (%d,%d,%t)`, 1, 3, true, start, end, ok)
+	}
+	lookFor = 2
+	endIdx = 10
+	if start, end, ok := day09.FindEmptySpace(&test, &lookFor, &endIdx); start != 2 && end != 4 && !ok {
+		t.Errorf(`Expected (%d,%d,%t) and got (%d,%d,%t)`, 1, 4, true, start, end, ok)
+	}
+	lookFor = 6
+	endIdx = 10
+	if start, end, ok := day09.FindEmptySpace(&test, &lookFor, &endIdx); start != -1 && end != -1 && ok {
+		t.Errorf(`Expected (%d,%d,%t) and got (%d,%d,%t)`, -1, -1, false, start, end, ok)
+	}
+}
+
+func TestFindBlockWithNegStart(t *testing.T) {
+	test := []int16{0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2}
+	maxIdx := -1
+	var id int16 = 2
+	if start, end, ok := day09.FindBlock(&test, &id, &maxIdx); start != 10 && end != 15 && !ok {
+		t.Errorf(`Expected (%d,%d,%t) and got (%d,%d,%t)`, 10, 15, true, start, end, ok)
+	}
+	id = 1
+	if start, end, ok := day09.FindBlock(&test, &id, &maxIdx); start != 3 && end != 6 && !ok {
+		t.Errorf(`Expected (%d,%d,%t) and got (%d,%d,%t)`, 3, 6, true, start, end, ok)
+	}
+	id = 6
+	if start, end, ok := day09.FindBlock(&test, &id, &maxIdx); start != -1 && end != -1 && ok {
+		t.Errorf(`Expected (%d,%d,%t) and got (%d,%d,%t)`, -1, -1, true, start, end, ok)
+	}
+}
+func TestCompactLess(t *testing.T) {
+	test := []byte("2333133121414131402")
+	expandedData := day09.ConvertInput(&test)
+	day09.CompactLess(&expandedData)
+	expected := []int16{0, 0, 9, 9, 2, 1, 1, 1, 7, 7, 7, -1, 4, 4, -1, 3, 3, 3, -1, -1, -1, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, -1, -1, -1, -1, 8, 8, 8, 8, -1, -1}
+	if !slices.Equal(expandedData, expected) {
+		t.Errorf(`Expected %v to be %v`, expandedData, expected)
+	}
+	if checkSum := day09.CheckSum(&expandedData); checkSum != 2858 {
+		t.Errorf(`Expected checksum %d to be %d`, checkSum, 2858)
+	}
+}

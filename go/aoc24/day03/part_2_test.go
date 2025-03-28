4635160b812c527c689188dc629bc483fe0fff14
donmahallem <donmahallem@users.noreply.github.com>
2025-03-28 07:47:12 +0100

    add tests for 24 day 03

--- go/aoc24/day03/part_2_test.go
+++ go/aoc24/day03/part_2_test.go
@@ -10,7 +10,7 @@ import (
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
-func TestHelloName(t *testing.T) {
+func TestNewDoReader(t *testing.T) {
 	const sourceData = "{\"id\": 10, \"name\": \"Pie\"}"
 	var data, _ = io.ReadAll(day03.NewDoReader(strings.NewReader(sourceData)))
 
@@ -18,12 +18,21 @@ func TestHelloName(t *testing.T) {
 		t.Errorf(`Expected %s to match %s`, string(data), sourceData)
 	}
 }
-func TestHelloName2(t *testing.T) {
+func TestNewDoReader2(t *testing.T) {
 	const sourceData = "asdfdo()yodon't()nodo()asdf"
 	const targetData = "asdfyoasdf"
 	var data, _ = io.ReadAll(day03.NewDoReader(strings.NewReader(sourceData)))
 
 	if i := strings.Compare(string(data), targetData); i != 0 {
 		t.Errorf(`Expected %s to match %s`, string(data), targetData)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	const sourceData = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+	var data = day03.Part2(strings.NewReader(sourceData))
+	expected := 48
+	if data != expected {
+		t.Errorf(`Expected %d to match %d`, data, expected)
 	}
 }

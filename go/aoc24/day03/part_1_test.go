d30eb430305398755b2029ae6b2a942781dc0c5a
ò_ó <donmahallem@users.noreply.github.com>
2025-03-28 07:49:48 +0100

    add tests for 24 day 03 (#34)

--- go/aoc24/day03/part_1_test.go
+++ go/aoc24/day03/part_1_test.go
@@ -8,13 +8,20 @@ import (
 	"github.com/donmahallem/aoc/aoc24/day03"
 )
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
-func TestHelloName32(t *testing.T) {
+func TestNewMulReader(t *testing.T) {
 	const sourceData = "mul(1,2)mul(1amul(2,3)"
 	var data, _ = io.ReadAll(day03.NewMulReader(strings.NewReader(sourceData)))
 
 	if i := strings.Compare(string(data), "8"); i != 0 {
 		t.Errorf(`Expected %s to match %s`, string(data), "8")
+	}
+}
+
+func TestPart1(t *testing.T) {
+	const sourceData = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+	var data = day03.Part1(strings.NewReader(sourceData))
+	expected := 161
+	if data != expected {
+		t.Errorf(`Expected %d to match %d`, data, expected)
 	}
 }

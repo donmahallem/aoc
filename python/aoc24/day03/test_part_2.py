06340c18cc15f5e37afd25fcd7e5e9dc9e203516
donmahallem <donmahallem@users.noreply.github.com>
2025-03-30 21:37:11 +0200

    add 24 day03 python tests

---
+++ python/aoc24/day03/test_part_2.py
@@ -0,0 +1,18 @@
+import unittest
+import io
+from aoc24.day03.part_2 import Part2
+
+testData = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+
+class Test2024Day02Part02(unittest.TestCase):
+    def test_result(self):
+        with io.StringIO() as f:
+            f.write(testData)
+            f.seek(0)
+            result = Part2(f)
+            self.assertEqual(result, 48, "The sum is wrong.")
+
+
+if __name__ == "__main__":
+    unittest.main()

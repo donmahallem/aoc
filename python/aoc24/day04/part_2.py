b26fdbf8ac2f66665b79a3020d0d134a43aa09c0
donmahallem <donmahallem@users.noreply.github.com>
2025-03-30 22:19:18 +0200

    add 24 day04 and day05 tests

--- python/aoc24/day04/part_2.py
+++ python/aoc24/day04/part_2.py
@@ -1,42 +1,28 @@
-import codecs
-import regex
-import numpy as np
+import typing
+import sys
 
-with codecs.open("data.txt", encoding="utf8") as f:
-    data = [a.strip() for a in f.readlines()]
-search_terms = "XMAS"
-zeilen = len(data)
-spalten = len(data[0])
+def Part2(input: typing.TextIO) -> int:
+    data = [a.strip() for a in input.readlines()]
+    zeilen = len(data)
+    spalten = len(data[0])
+    count = 0
+    dirs = [[[-1, -1, 1, 1], [1, 1, -1, -1]], [[-1, 1, 1, -1], [1, -1, -1, 1]]]
+    for y in range(1, zeilen - 1):
+        for x in range(1, spalten - 1):
+            matches = 0
+            if data[y][x] == "A":
+                for axis in dirs:
+                    for dirx1, diry1, dirx2, diry2 in axis:
+                        if (
+                            data[y + diry1][x + dirx1] == "M"
+                            and data[y + diry2][x + dirx2] == "S"
+                        ):
+                            matches += 1
+                            # Only max one occurence per axis
+                            break
+            if matches == 2:
+                count += 1
+    return count
 
-
-def checkDir(x, y, dirx, diry, term):
-    if x + (dirx * len(term)) + 1 < 0 or x + (dirx * len(term)) > spalten:
-        return False
-    elif y + (diry * len(term)) + 1 < 0 or y + (diry * len(term)) > zeilen:
-        return False
-    for i in range(len(term)):
-        if x == 3 and y == 9 and dirx == -1:
-            print(data[y + diry * i][x + dirx * i], term[i])
-        if data[y + diry * i][x + dirx * i] != term[i]:
-            return False
-    return True
-
-
-count = 0
-dirs = [[[-1, -1, 1, 1], [1, 1, -1, -1]], [[-1, 1, 1, -1], [1, -1, -1, 1]]]
-for y in range(1, zeilen - 1):
-    for x in range(1, spalten - 1):
-        matches = 0
-        if data[y][x] == "A":
-            for axis in dirs:
-                for dirx1, diry1, dirx2, diry2 in axis:
-                    if (
-                        data[y + diry1][x + dirx1] == "M"
-                        and data[y + diry2][x + dirx2] == "S"
-                    ):
-                        matches += 1
-                        # Only max one occurence per axis
-                        break
-        if matches == 2:
-            count += 1
-print(count)
+if __name__ == "__main__":
+    Part2(sys.stdin)
---
+++ python/aoc24/day05/test_part_2.py
@@ -0,0 +1,17 @@
+import unittest
+import io
+from aoc24.day05.part_2 import Part2
+
+from .test_part_1 import testData
+
+class Test2024Day05Part02(unittest.TestCase):
+    def test_result(self):
+        with io.StringIO() as f:
+            f.write(testData)
+            f.seek(0)
+            result = Part2(f)
+            self.assertEqual(result, 123, "The sum is wrong.")
+
+
+if __name__ == "__main__":
+    unittest.main()

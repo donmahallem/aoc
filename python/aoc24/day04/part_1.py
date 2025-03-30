e3c91df5058ae0e2519ac2e7f4dbf316fbe042e9
ò_ó <donmahallem@users.noreply.github.com>
2025-03-30 22:21:52 +0200

    add 24 day 04 and day 05 tests (#46)
    * add 24 day04 and day05 tests
    
    * fix lint

--- python/aoc24/day04/part_1.py
+++ python/aoc24/day04/part_1.py
@@ -1,31 +1,33 @@
-import codecs
-import regex
-import numpy as np
-
-with codecs.open("data.txt", encoding="utf8") as f:
-    data = [a.strip() for a in f.readlines()]
-search_terms = "XMAS"
-zeilen = len(data)
-spalten = len(data[0])
-count = 0
+import sys
+import typing
 
 
-def checkDir(x, y, dirx, diry, term):
-    if x + (dirx * len(term)) + 1 < 0 or x + (dirx * len(term)) > spalten:
-        return False
-    elif y + (diry * len(term)) + 1 < 0 or y + (diry * len(term)) > zeilen:
-        return False
-    for i in range(len(term)):
-        if data[y + diry * i][x + dirx * i] != term[i]:
+def Part1(input: typing.TextIO) -> int:
+    data = [a.strip() for a in input.readlines()]
+    search_terms = "XMAS"
+    zeilen = len(data)
+    spalten = len(data[0])
+    count = 0
+
+    def checkDir(x, y, dirx, diry, term):
+        if x + (dirx * len(term)) + 1 < 0 or x + (dirx * len(term)) > spalten:
             return False
-    return True
+        elif y + (diry * len(term)) + 1 < 0 or y + (diry * len(term)) > zeilen:
+            return False
+        for i in range(len(term)):
+            if data[y + diry * i][x + dirx * i] != term[i]:
+                return False
+        return True
+
+    count = 0
+    dirs = [[1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1]]
+    for y in range(0, zeilen):
+        for x in range(spalten):
+            for xdir, ydir in dirs:
+                if checkDir(x, y, xdir, ydir, search_terms):
+                    count += 1
+    return count
 
 
-count = 0
-dirs = [[1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1]]
-for y in range(0, zeilen):
-    for x in range(spalten):
-        for xdir, ydir in dirs:
-            if checkDir(x, y, xdir, ydir, search_terms):
-                count += 1
-print(count)
+if __name__ == "__main__":
+    Part1(sys.stdin)
--- python/aoc24/day05/part_1.py
+++ python/aoc24/day05/part_1.py
@@ -1,31 +1,35 @@
-import codecs
+import typing
+import sys
 
-with codecs.open("data.txt", encoding="utf8") as f:
-    data = [a.strip() for a in f.readlines()]
+
+def Part1(input: typing.TextIO) -> int:
+    data = [a.strip() for a in input.readlines()]
     data = "\n".join(data)
-ordering, pages = data.split("\n\n")
-ordering = [tuple([int(a) for a in row.split("|")]) for row in ordering.split("\n")]
-pages = [[int(a) for a in row.split(",")] for row in pages.split("\n")]
+    ordering, pages = data.split("\n\n")
+    ordering = [tuple([int(a) for a in row.split("|")]) for row in ordering.split("\n")]
+    pages = [[int(a) for a in row.split(",")] for row in pages.split("\n")]
 
-rules = dict()
-for a, b in ordering:
-    if a not in rules:
-        rules[a] = [b]
-    else:
-        rules[a].append(b)
+    rules = dict()
+    for a, b in ordering:
+        if a not in rules:
+            rules[a] = [b]
+        else:
+            rules[a].append(b)
+
+    def validRow(row):
+        valid = True
+        for idx in range(len(row) - 1):
+            if not (row[idx] in rules and row[idx + 1] in rules[row[idx]]):
+                valid = False
+                break
+        return valid
+
+    valid_pages = []
+    for page in pages:
+        if validRow(page):
+            valid_pages.append(page)
+    return sum([page[len(page) // 2] for page in valid_pages])
 
 
-def validRow(row):
-    valid = True
-    for idx in range(len(row) - 1):
-        if not (row[idx] in rules and row[idx + 1] in rules[row[idx]]):
-            valid = False
-            break
-    return valid
-
-
-valid_pages = []
-for page in pages:
-    if validRow(page):
-        valid_pages.append(page)
-print(sum([page[len(page) // 2] for page in valid_pages]))
+if __name__ == "__main__":
+    Part1(sys.stdin)

5e82f4a20fbb08c76ba89190b00c25722e913aab
donmahallem <donmahallem@users.noreply.github.com>
2025-03-30 18:22:19 +0200

    add 24 day02 tests

---
+++ go/aoc24/day02/part_1_test.go
@@ -0,0 +1,22 @@
+package day02_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/donmahallem/aoc/aoc24/day02"
+)
+
+const testData string = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+
+func TestPart1(t *testing.T) {
+	result := day02.Part1(strings.NewReader(testData))
+	if result != 2 {
+		t.Errorf(`Expected %d to be %d`, result, 2)
+	}
+}

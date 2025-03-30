2dfdf3f8a2f5be558a89d0d59f0693232294879c
donmahallem <4698322+donmahallem@users.noreply.github.com>
2025-03-30 19:37:49 +0000

    OMG, have you seen those snek pics? 🐍✨ Like, for real, these slithery bois are serving looks! 💅 1. This one’s got those iridescent scales that shine like a rainbow in the sun. 🌈✨ Legit, it’s like Mother Nature decided to flex on us. #SnekGoals 2. Spotted pattern? Check. Regal AF? Double check. This snek is basically the royalty of the reptile world. 👑🐍 Bow down, peasants. 3. Snek #3: Albino snek with those red eyes? Bruh, it’s like a mythical creature straight outta a fantasy novel. 🧙‍♂️✨ #SnekMagic 4. Green tree python just chilling on a branch, looking like it owns the place. 🌿🐍 This snek is the definition of “I woke up like this.” #Flawless
    And let’s not forget their sassy little tongues flicking out like they’re tasting the air for drama. 👅💅 These sneks are out here living their best lives, making us all look basic in comparison.
    
    So, next time you see a snek, remember: they’re not just reptiles, they’re fashion icons. 🐍✨ #SnekLife #SlitherAndShine
    
    Stay fabulous, sneks. We stan. 💖🐍

--- python/aoc24/day03/part_1.py
+++ python/aoc24/day03/part_1.py
@@ -6,9 +6,10 @@ import typing
 def Part1(input: typing.TextIO) -> int:
     data = input.readlines()
     data = "".join(data)
-    comp = re.compile(r'mul\((\d+)\,(\d+)\)', flags=re.MULTILINE)
+    comp = re.compile(r"mul\((\d+)\,(\d+)\)", flags=re.MULTILINE)
     findings = comp.findall(data)
-    return (sum([int(a) * int(b) for a, b in findings]))
+    return sum([int(a) * int(b) for a, b in findings])
+
 
 if __name__ == "__main__":
     Part1(sys.stdin)
--- python/aoc24/day03/part_2.py
+++ python/aoc24/day03/part_2.py
@@ -2,12 +2,13 @@ import typing
 import sys
 import re
 
+
 def Part2(input: typing.TextIO) -> int:
     data = input.readlines()
     data = "\n".join(data)
     enabled = True
-    comp = re.compile(r'do(n\'t)?\(\)', flags=re.MULTILINE)
-    comp2 = re.compile(r'mul\((\d+)\,(\d+)\)', flags=re.MULTILINE)
+    comp = re.compile(r"do(n\'t)?\(\)", flags=re.MULTILINE)
+    comp2 = re.compile(r"mul\((\d+)\,(\d+)\)", flags=re.MULTILINE)
     total = 0
     for item in comp.split(data):
         if item == "n't":
@@ -19,6 +20,7 @@ def Part2(input: typing.TextIO) -> int:
             findings = comp2.findall(item)
             total += sum([int(a) * int(b) for a, b in findings])
     return total
+
 
 if __name__ == "__main__":
     Part2(sys.stdin)

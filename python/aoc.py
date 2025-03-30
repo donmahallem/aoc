b052b6e82f841bd06161cc433c08bdb775302cec
donmahallem <donmahallem@users.noreply.github.com>
2025-03-30 23:12:53 +0200

    add root py method

--- python/aoc.py
+++ python/aoc.py
@@ -1,17 +1,34 @@
 import importlib
 import importlib.util
+import argparse
+import sys
 
-for year in range(24,25):
-    yearStr=f"aoc{year}"
-    for day in range(1,26):
-        dayStr=f"day{day:02}"
-        for part in range(1,3):
-            compound=f"{yearStr}.{dayStr}.part_{part}"
-            try:
-                spam_spec = importlib.util.find_spec(compound)
-                if spam_spec:
-                    mod=importlib.import_module(compound)
-                    if hasattr(mod, f'Part{part}'):
-                        print(compound ,"exists",part)
-            except:
-                print("An exception occurred")
+
+def getPart(year, day, part):
+    compound = f"aoc{year}.day{day:02}.part_{part}"
+    try:
+        spam_spec = importlib.util.find_spec(compound)
+        if spam_spec:
+            mod = importlib.import_module(compound)
+            partName = f"Part{part}"
+            if hasattr(mod, partName):
+                return getattr(mod,partName)
+    except BaseException as error:
+        print(error)
+        return None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="AOC Solver", description="Solves Aoc", epilog="Text at the bottom of help"
+    )
+    parser.add_argument("year", type=int,choices=[24])  # positional argument
+    parser.add_argument("day", type=int,choices=list(range(1,25)))  # option that takes a value
+    parser.add_argument("part", type=int,choices=[1,2])
+    args = parser.parse_args()
+    print("Requested Year:", args.year, "Day:", args.day, "Part:", args.part)
+
+    solver = getPart(args.year, args.day, args.part)
+    if solver:
+        solver(sys.stdin)
+    else:
+        print("Could not find requested solver")

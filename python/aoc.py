6f13933e845b3504ffb977d927c466a1d5dcc0ba
donmahallem <donmahallem@users.noreply.github.com>
2025-03-30 23:13:42 +0200

    fix style

--- python/aoc.py
+++ python/aoc.py
@@ -12,18 +12,21 @@ def getPart(year, day, part):
             mod = importlib.import_module(compound)
             partName = f"Part{part}"
             if hasattr(mod, partName):
-                return getattr(mod,partName)
+                return getattr(mod, partName)
     except BaseException as error:
         print(error)
         return None
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="AOC Solver", description="Solves Aoc", epilog="Text at the bottom of help"
     )
-    parser.add_argument("year", type=int,choices=[24])  # positional argument
-    parser.add_argument("day", type=int,choices=list(range(1,25)))  # option that takes a value
-    parser.add_argument("part", type=int,choices=[1,2])
+    parser.add_argument("year", type=int, choices=[24])  # positional argument
+    parser.add_argument(
+        "day", type=int, choices=list(range(1, 25))
+    )  # option that takes a value
+    parser.add_argument("part", type=int, choices=[1, 2])
     args = parser.parse_args()
     print("Requested Year:", args.year, "Day:", args.day, "Part:", args.part)
 

80ce2848b97a4c1317c788ce31c8ffe0c3507264
donmahallem <4698322+donmahallem@users.noreply.github.com>
2025-03-30 18:58:27 +0000

    OMG, have you seen those snek pics? 🐍✨ Like, for real, these slithery bois are serving looks! 💅 1. This one’s got those iridescent scales that shine like a rainbow in the sun. 🌈✨ Legit, it’s like Mother Nature decided to flex on us. #SnekGoals 2. Spotted pattern? Check. Regal AF? Double check. This snek is basically the royalty of the reptile world. 👑🐍 Bow down, peasants. 3. Snek #3: Albino snek with those red eyes? Bruh, it’s like a mythical creature straight outta a fantasy novel. 🧙‍♂️✨ #SnekMagic 4. Green tree python just chilling on a branch, looking like it owns the place. 🌿🐍 This snek is the definition of “I woke up like this.” #Flawless
    And let’s not forget their sassy little tongues flicking out like they’re tasting the air for drama. 👅💅 These sneks are out here living their best lives, making us all look basic in comparison.
    
    So, next time you see a snek, remember: they’re not just reptiles, they’re fashion icons. 🐍✨ #SnekLife #SlitherAndShine
    
    Stay fabulous, sneks. We stan. 💖🐍

--- python/aoc24/day01/test_part_1.py
+++ python/aoc24/day01/test_part_1.py
@@ -2,19 +2,22 @@ import unittest
 import io
 from aoc24.day01.part_1 import Part1
 
-testData="""3   4
+testData = """3   4
 4   3
 2   5
 1   3
 3   9
 3   3"""
+
+
 class Test2024Day01Part01(unittest.TestCase):
     def test_result(self):
         with io.StringIO() as f:
             f.write(testData)
             f.seek(0)
             result = Part1(f)
-            self.assertEqual(result, 11, 'The sum is wrong.')
+            self.assertEqual(result, 11, "The sum is wrong.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()

b39098c8d22b6042b32d3f8840eb186f9e33fcb1
donmahallem <donmahallem@users.noreply.github.com>
2025-03-30 09:48:10 +0200

    add 23 day04 part2

---
+++ go/aoc23/day04/part_2.go
@@ -0,0 +1,49 @@
+package day04
+
+import (
+	"bufio"
+	"io"
+
+	"github.com/donmahallem/aoc/aoc_utils"
+)
+
+func SumArray(a []int) int {
+	val := 0
+	for i := range a {
+		val += a[i]
+	}
+	return val
+}
+
+func CountTickets(valid *[]int, idx int) int {
+	counter := make(map[int]int)
+	add := func(idx, count int) {
+		//fmt.Printf("Add %d to %d. Was %d\n", count, idx, counter[idx])
+		if val, ok := counter[idx]; ok {
+			counter[idx] = val + count
+		} else {
+			counter[idx] = count
+		}
+	}
+	for i := range *valid {
+		for j := i + 1; j < aoc_utils.Min(i+1+(*valid)[i], len(*valid)); j++ {
+			add(j, 1+counter[i])
+		}
+		add(i, 1)
+	}
+	//fmt.Printf("%v\n", counter)
+	score := 0
+	for key := range counter {
+		score += counter[key]
+	}
+	return score
+}
+func Part2(in io.Reader) int {
+	s := bufio.NewScanner(in)
+	valid := make([]int, 0)
+	for s.Scan() {
+		_, a, b := ParseLine(s.Bytes())
+		valid = append(valid, CountWinnings(a, b))
+	}
+	return CountTickets(&valid, 0)
+}

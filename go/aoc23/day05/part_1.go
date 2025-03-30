fe776f89bcca21365d42108aa562f2b16e7e8a66
donmahallem <donmahallem@users.noreply.github.com>
2025-03-30 17:42:05 +0200

    add 23 day05 part1

--- go/aoc23/day05/part_1.go
+++ go/aoc23/day05/part_1.go
@@ -2,7 +2,6 @@ package day05
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"math"
 	"strconv"
@@ -18,7 +17,7 @@ type Almanac struct {
 	WaterToLight,
 	LightToTemperature,
 	TemperatureToHumidity,
-	HumidityToLocation map[int]int
+	HumidityToLocation [][]int
 	Seeds []int
 }
 
@@ -50,8 +49,7 @@ func TranslateMap(line *[]int, data *map[int]int) {
 func ParseAlmanac(in io.Reader) Almanac {
 	s := bufio.NewScanner(in)
 	alma := Almanac{}
-	var target *map[int]int
-	data := make([]int, 0, 3)
+	var target *[][]int
 	for s.Scan() {
 		line := s.Text()
 		if strings.HasPrefix(line, "seeds:") {
@@ -62,65 +60,60 @@ func ParseAlmanac(in io.Reader) Almanac {
 		} else if len(line) == 0 {
 			continue
 		} else if strings.HasPrefix(line, SEED_TO_SOIL) {
-			alma.SeedToSoil = make(map[int]int)
+			alma.SeedToSoil = make([][]int, 0)
 			target = &alma.SeedToSoil
 		} else if strings.HasPrefix(line, SOIL_TO_FERTILIZER) {
-			alma.SoilToFertilizer = make(map[int]int)
+			alma.SoilToFertilizer = make([][]int, 0)
 			target = &alma.SoilToFertilizer
 		} else if strings.HasPrefix(line, FERTILIZER_TO_WATER) {
-			alma.FertilizerToWater = make(map[int]int)
+			alma.FertilizerToWater = make([][]int, 0)
 			target = &alma.FertilizerToWater
 		} else if strings.HasPrefix(line, WATER_TO_LIGHT) {
-			alma.WaterToLight = make(map[int]int)
+			alma.WaterToLight = make([][]int, 0)
 			target = &alma.WaterToLight
 		} else if strings.HasPrefix(line, LIGHT_TO_TEMPERATURE) {
-			alma.LightToTemperature = make(map[int]int)
+			alma.LightToTemperature = make([][]int, 0)
 			target = &alma.LightToTemperature
 		} else if strings.HasPrefix(line, TEMPERATURE_TO_HUMIDITY) {
-			alma.TemperatureToHumidity = make(map[int]int)
+			alma.TemperatureToHumidity = make([][]int, 0)
 			target = &alma.TemperatureToHumidity
 		} else if strings.HasPrefix(line, HUMIDITY_TO_LOCATION) {
-			alma.HumidityToLocation = make(map[int]int)
+			alma.HumidityToLocation = make([][]int, 0)
 			target = &alma.HumidityToLocation
 		} else {
-			data = data[:0]
+			data := make([]int, 0, 3)
 			ParseMap(&line, &data)
-			TranslateMap(&data, target)
+			*target = append(*target, data)
+			//TranslateMap(&data, target)
 		}
 
 	}
 	return alma
 }
 
+func FindPos(k *[][]int, pos int) int {
+	for _, row := range *k {
+		if pos >= row[1] && pos < row[1]+row[2] {
+			return pos + row[0] - row[1]
+		}
+	}
+	return pos
+}
+
 func GetPosition(a *Almanac, start int) int {
 	pos := start
-	if pos1, ok := (*a).SeedToSoil[pos]; ok {
-		pos = pos1
-	}
-	if pos1, ok := (*a).SoilToFertilizer[pos]; ok {
-		pos = pos1
-	}
-	if pos1, ok := (*a).FertilizerToWater[pos]; ok {
-		pos = pos1
-	}
-	if pos1, ok := (*a).WaterToLight[pos]; ok {
-		pos = pos1
-	}
-	if pos1, ok := (*a).LightToTemperature[pos]; ok {
-		pos = pos1
-	}
-	if pos1, ok := (*a).TemperatureToHumidity[pos]; ok {
-		pos = pos1
-	}
-	if pos1, ok := (*a).HumidityToLocation[pos]; ok {
-		pos = pos1
-	}
+	pos = FindPos(&(*a).SeedToSoil, pos)
+	pos = FindPos(&(*a).SoilToFertilizer, pos)
+	pos = FindPos(&(*a).FertilizerToWater, pos)
+	pos = FindPos(&(*a).WaterToLight, pos)
+	pos = FindPos(&(*a).LightToTemperature, pos)
+	pos = FindPos(&(*a).TemperatureToHumidity, pos)
+	pos = FindPos(&(*a).HumidityToLocation, pos)
 	return pos
 }
 
 func Part1(in io.Reader) int {
 	almanac := ParseAlmanac(in)
-	fmt.Println("Parsed almanac")
 	lowest := math.MaxInt
 	for _, seed := range almanac.Seeds {
 		lowest = aoc_utils.Min(lowest, GetPosition(&almanac, seed))

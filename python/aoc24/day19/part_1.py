7b56881e060218bebf6b0f330430dc1653ff4ca3
donmahallem <donmahallem@users.noreply.github.com>
2025-03-31 21:31:20 +0200

    add day19 24 tests

--- python/aoc24/day19/part_1.py
+++ python/aoc24/day19/part_1.py
@@ -1,29 +1,24 @@
-import codecs
+import typing
 from functools import cache
+from .shared import parseInput
 
-test_data = False
+def Part1(input: typing.TextIO) -> int:
+    available_patterns, needed_patterns =parseInput(input)
 
-with codecs.open("data.txt" if test_data else "data2.txt", encoding="utf8") as f:
-    data = f.readlines()
-
-available_patterns = [line.strip() for line in data[0].strip().split(",")]
-needed_patterns = [line.strip() for line in data[2:]]
-
-
-@cache
-def checkPattern2(pattern):
-    for test_pattern in available_patterns:
-        if test_pattern == pattern:
-            return True
-        elif pattern[0 : len(test_pattern)] == test_pattern:
-            if checkPattern2(pattern[len(test_pattern) :]):
+    @cache
+    def checkPattern2(pattern):
+        for test_pattern in available_patterns:
+            if test_pattern == pattern:
                 return True
-    return False
+            elif pattern[0 : len(test_pattern)] == test_pattern:
+                if checkPattern2(pattern[len(test_pattern) :]):
+                    return True
+        return False
 
 
-results = 0
-for p in needed_patterns:
-    if checkPattern2(p):
-        results += 1
+    results = 0
+    for p in needed_patterns:
+        if checkPattern2(p):
+            results += 1
 
-print(results)
+    return results

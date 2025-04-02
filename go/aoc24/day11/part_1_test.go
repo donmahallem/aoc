40568160b7f643fac6b4ec5b15e2add7371a8ec1
donmahallem <donmahallem@users.noreply.github.com>
2025-04-02 20:28:36 +0200

    add go tests

--- go/aoc24/day11/part_1_test.go
+++ go/aoc24/day11/part_1_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/donmahallem/aoc/aoc24/day11"
 )
 
+const testData string = "125 17"
+
 func TestParseLine(t *testing.T) {
 	test := "125 17"
 	expected := []int{125, 17}
@@ -41,5 +43,12 @@ func TestSplitStones(t *testing.T) {
 	f1 = day11.SplitStones([]int{125, 17}, 25)
 	if f1 != 55312 {
 		t.Errorf(`Expected %d to match %d`, f1, 55312)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	test := day11.Part1(strings.NewReader(testData))
+	if test != 55312 {
+		t.Errorf(`Expected %d to match 55312`, test)
 	}
 }

3880f7d40ee42c836d690086ecf87502764f64c8
donmahallem <donmahallem@users.noreply.github.com>
2025-04-02 16:42:34 +0200

    use functools cache

--- python/aoc24/day11/part_1.py
+++ python/aoc24/day11/part_1.py
@@ -1,35 +1,30 @@
 import typing
 import math
+from functools import cache
 
 def Part1(input: typing.TextIO,blinks:int=25) -> int:
     data = [int(a) for a in input.readline().split(" ")]
-    cache = dict()
     summe = 0
     for i in data:
-        summe += christmas_tree(i, blinks,cache)
+        summe += christmas_tree(i, blinks)
     return summe
 
-
-def christmas_tree(item, depth,cache):
+@cache
+def christmas_tree(item, depth):
     if depth == 0:
         return 1
-    elif (item, depth) in cache:
-        return cache[((item, depth))]
     if item == 0:
-        result = christmas_tree(1, depth - 1,cache)
-        cache[(item, depth)] = result
+        result = christmas_tree(1, depth - 1)
         return result
     item_length = int(math.log10(item)) + 1
     if item_length % 2 == 0:
         split_barrier = 10 ** (item_length / 2)
-        result = christmas_tree(int(item / split_barrier), depth - 1,cache) + christmas_tree(
-            int(item % split_barrier), depth - 1,cache
+        result = christmas_tree(int(item / split_barrier), depth - 1) + christmas_tree(
+            int(item % split_barrier), depth - 1
         )
-        cache[(item, depth)] = result
         return result
     else:
-        result = christmas_tree(item * 2024, depth - 1,cache)
-        cache[(item, depth)] = result
+        result = christmas_tree(item * 2024, depth - 1)
         return result
 
 
--- python/aoc24/day11/part_2.py
+++ python/aoc24/day11/part_2.py
@@ -2,5 +2,5 @@ import typing
 from .part_1 import Part1
 
 
-def Part2(input: typing.TextIO,blinks:int=25) -> int:
-    return Part1(input,75)
+def Part2(input: typing.TextIO,blinks:int=75) -> int:
+    return Part1(input,blinks)

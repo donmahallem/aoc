f41eb8ca49dfc66d90842a46c2605adbea75a4e3
donmahallem <donmahallem@users.noreply.github.com>
2025-04-02 16:40:50 +0200

    add initial tests

--- python/aoc24/day11/part_1.py
+++ python/aoc24/day11/part_1.py
@@ -1,38 +1,35 @@
-import codecs
+import typing
 import math
 
-with codecs.open("data.txt", encoding="utf8") as f:
-    data = [[int(num) for num in a.strip().split()] for a in f.readlines()]
+def Part1(input: typing.TextIO,blinks:int=25) -> int:
+    data = [int(a) for a in input.readline().split(" ")]
+    cache = dict()
+    summe = 0
+    for i in data:
+        summe += christmas_tree(i, blinks,cache)
+    return summe
 
-data = data[0]
-cache = dict()
 
-
-def christmas_tree(item, depth):
+def christmas_tree(item, depth,cache):
     if depth == 0:
         return 1
     elif (item, depth) in cache:
         return cache[((item, depth))]
     if item == 0:
-        result = christmas_tree(1, depth - 1)
+        result = christmas_tree(1, depth - 1,cache)
         cache[(item, depth)] = result
         return result
     item_length = int(math.log10(item)) + 1
     if item_length % 2 == 0:
         split_barrier = 10 ** (item_length / 2)
-        result = christmas_tree(int(item / split_barrier), depth - 1) + christmas_tree(
-            int(item % split_barrier), depth - 1
+        result = christmas_tree(int(item / split_barrier), depth - 1,cache) + christmas_tree(
+            int(item % split_barrier), depth - 1,cache
         )
         cache[(item, depth)] = result
         return result
     else:
-        result = christmas_tree(item * 2024, depth - 1)
+        result = christmas_tree(item * 2024, depth - 1,cache)
         cache[(item, depth)] = result
         return result
 
 
-blink_num = 25
-summe = 0
-for i in data:
-    summe += christmas_tree(i, blink_num)
-print(summe)

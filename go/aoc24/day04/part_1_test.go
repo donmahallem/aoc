7e6e7b54fb7063708929009aad4339f794da9064
ò_ó <donmahallem@users.noreply.github.com>
2025-04-02 13:35:49 +0200

    add test cases for go day 4 2024 (#69)

--- go/aoc24/day04/part_1_test.go
+++ go/aoc24/day04/part_1_test.go
@@ -1,10 +1,22 @@
 package day04_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/donmahallem/aoc/aoc24/day04"
 )
+
+const testData string = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
 
 var testDataString = [][]byte{[]byte("MMMSXXMASM"),
 	[]byte("MSAMXMSMSA"),
@@ -19,19 +31,26 @@ var testDataString = [][]byte{[]byte("MMMSXXMASM"),
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
-func Test24Day04Part1(t *testing.T) {
+func TestCheckBlock1(t *testing.T) {
 	result := day04.CheckBlock(testDataString)
 	if result != 18 {
 		t.Errorf(`Expected %d to match %d`, result, 18)
 	}
 }
 
-func TestCheckBlock(t *testing.T) {
+func TestCheckBlock2(t *testing.T) {
 	count := 0
 	for i := 0; i < len(testDataString); i++ {
 		count += day04.CheckBlock(testDataString[max(0, i-3) : i+1])
 	}
 	if count != 30 {
 		t.Errorf(`Expected %d to match %d`, count, 30)
+	}
+}
+
+func Test24Day04Part1(t *testing.T) {
+
+	if result := day04.Part1(strings.NewReader(testData)); result != 18 {
+		t.Errorf(`Expected %d to match %d`, result, 18)
 	}
 }
--- go/aoc24/day04/part_2_test.go
+++ go/aoc24/day04/part_2_test.go
@@ -1,6 +1,7 @@
 package day04_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/donmahallem/aoc/aoc24/day04"
@@ -8,9 +9,16 @@ import (
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
-func Test24Day04Part2(t *testing.T) {
+func TestCheckMasBlock(t *testing.T) {
 	result := day04.CheckMasBlock(testDataString)
 	if result != 9 {
+		t.Errorf(`Expected %d to match %d`, result, 9)
+	}
+}
+
+func Test24Day04Part2(t *testing.T) {
+
+	if result := day04.Part2(strings.NewReader(testData)); result != 9 {
 		t.Errorf(`Expected %d to match %d`, result, 9)
 	}
 }

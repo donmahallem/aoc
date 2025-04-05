b8a76a5f5e54aa271fd6630cfff6e20243ff178f
donmahallem <donmahallem@users.noreply.github.com>
2025-04-05 18:19:35 +0200

    use point for day12

--- go/aoc24/day12/part_1_test.go
+++ go/aoc24/day12/part_1_test.go
@@ -21,7 +21,7 @@ MIIISIJEEE
 MMMISSJEEE`
 
 func TestCountEdges(t *testing.T) {
-	test := [][2]int{{2, 2}, {2, 3}, {3, 3}}
+	test := []day12.Point{{Y: 2, X: 2}, {Y: 2, X: 3}, {Y: 3, X: 3}}
 	result := day12.CountEdges(test)
 	if result != 8 {
 		t.Errorf(`Expected %d to match %d`, result, 8)
@@ -31,7 +31,7 @@ func TestCountEdges(t *testing.T) {
 func TestFindNeighbours(t *testing.T) {
 	test, _ := aoc_utils.LoadField(strings.NewReader(testData))
 	result := day12.FindNeighbours(test, 0, 4)
-	expected := [][2]int{{0, 5}, {0, 4}, {1, 4}, {1, 5}}
+	expected := []day12.Point{{Y: 0, X: 5}, {Y: 0, X: 4}, {Y: 1, X: 4}, {Y: 1, X: 5}}
 	if len(result) != 4 {
 		t.Errorf(`Expected result to have a length of 4 not %d`, len(result))
 	}

44ae5de4621b0d11d973fb395e56677c3067cf8e
donmahallem <donmahallem@users.noreply.github.com>
2025-04-09 22:44:54 +0200

    remove broken test

--- go/aoc24/day17/part_2_test.go
+++ go/aoc24/day17/part_2_test.go
@@ -1,7 +1,6 @@
 package day17_test
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 
@@ -13,19 +12,6 @@ Register B: 0
 Register C: 0
 
 Program: 0,3,5,4,3,0`
-
-func TestSearchForNeutralElement(t *testing.T) {
-	reg := day17.Register{0, 0, 0}
-	prog_target := day17.Program{1}
-	prog := day17.Program{2, 1}
-	for !day17.SearchForNeutralElement(&reg, &prog, &prog_target) {
-		reg[0]++
-	}
-	if reg[0] > 0 {
-		t.Errorf(`Expected %d to match false`, reg[0])
-	}
-	fmt.Printf("%v - %v - %v\n", reg, prog_target, prog)
-}
 
 func TestPart2_testData2(t *testing.T) {
 	result := day17.Part2(strings.NewReader(testData2))

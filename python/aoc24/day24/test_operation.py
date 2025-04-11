2ddb66b4980caf670da275b6cc17ab2f562d79fd
donmahallem <donmahallem@users.noreply.github.com>
2025-04-11 23:24:45 +0200

    WIP

--- python/aoc24/day24/test_operation.py
+++ python/aoc24/day24/test_operation.py
@@ -4,32 +4,32 @@ from .operation import Operation
 class OperatorTests(unittest.TestCase):
 
     def test_and_operator(self):
-        obj=Operation([True,"AND",False,"out"])
+        obj=Operation([True,False],"AND","out")
         self.assertFalse(obj(), "The sum is wrong.")
-        obj=Operation([True,"AND",True,"out"])
+        obj=Operation([True,True],"AND","out")
         self.assertTrue(obj(), "The sum is wrong.")
 
     def test_or_operator(self):
-        obj=Operation([False,"OR",False,"out"])
+        obj=Operation([False,False],"OR","out")
         self.assertFalse(obj(), "False and False should be False")
-        obj=Operation([True,"OR",False,"out"])
+        obj=Operation([True,False],"OR","out")
         self.assertTrue(obj(), "True | False = True")
-        obj=Operation([True,"OR",True,"out"])
+        obj=Operation([True,True],"OR","out")
         self.assertTrue(obj(), "The sum is wrong.")
 
     def test_xor_operator(self):
-        obj=Operation([False,"XOR",False,"out"])
+        obj=Operation([False,False],"XOR","out")
         self.assertFalse(obj(), "False ^ False should be False")
-        obj=Operation([True,"XOR",False,"out"])
+        obj=Operation([True,False],"XOR","out")
         self.assertTrue(obj(), "True ^ False = True")
-        obj=Operation([True,"XOR",True,"out"])
+        obj=Operation([True,True],"XOR","out")
         self.assertFalse(obj(), "True ^ True should be False")
     def test_call_operator(self):
-        obj=Operation([False,"OR",False,"out"])
+        obj=Operation([False,False],"OR","out")
         self.assertFalse(True&obj(), "True & False should be True")
-        obj=Operation([False,"OR",False,"out"])
+        obj=Operation([False,False],"OR","out")
         self.assertFalse(False|obj(), "True | False should be True")
-        obj=Operation([False,"OR",False,"out"])
+        obj=Operation([False,False],"OR","out")
         self.assertFalse(False^obj(), "True ^ False should be True")
 
 

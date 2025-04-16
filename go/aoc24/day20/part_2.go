2e2041fba0b4b44fee17f655871baaf7db75ab96
donmahallem <donmahallem@users.noreply.github.com>
2025-04-17 00:23:42 +0200

    add go day 20

--- go/aoc24/day20/part_2.go
+++ go/aoc24/day20/part_2.go
@@ -1,1 +1,27 @@
 package day20
+
+import (
+	"io"
+)
+
+func CountCheats2(racewayPoints *[]Point, minSavings int, maxCheatDistance int) int {
+	cheatCount := 0
+	for leftIdx := range len(*racewayPoints) - minSavings {
+		for rightIdx := len(*racewayPoints) - 1; leftIdx+minSavings < rightIdx; rightIdx-- {
+			dst := (*racewayPoints)[leftIdx].DistanceManhatten((*racewayPoints)[rightIdx])
+			if dst < 2 || dst > maxCheatDistance {
+				continue
+			}
+			savings := rightIdx - leftIdx - dst
+			if savings >= minSavings {
+				cheatCount++
+			}
+		}
+	}
+	return cheatCount
+}
+
+func Part2(in io.Reader) int {
+	patterns := ParseInput(in)
+	return CountCheats2(patterns, 100, 20)
+}
---
+++ go/aoc24/day20/part_2_test.go
@@ -0,0 +1,22 @@
+package day20_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/donmahallem/aoc/aoc24/day20"
+)
+
+func TestCountCheats2(t *testing.T) {
+	patterns := day20.ParseInput(strings.NewReader(testData))
+	if result := day20.CountCheats2(patterns, 50, 20); result != 285 {
+		t.Errorf(`Expected %d to save %d steps. Not %d`, 2, 285, result)
+	}
+}
+
+func BenchmarkCountCheats2(b *testing.B) {
+	patterns := day20.ParseInput(strings.NewReader(testData))
+	for b.Loop() {
+		day20.CountCheats2(patterns, 50, 20)
+	}
+}

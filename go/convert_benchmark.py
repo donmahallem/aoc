c402264edba1aa2c21a75fc04af1296d5ff2a513
donmahallem <donmahallem@users.noreply.github.com>
2025-05-15 10:54:48 +0200

    ci(github): fck sneeky floats

--- go/convert_benchmark.py
+++ go/convert_benchmark.py
@@ -6,9 +6,10 @@ KEY_ACTION="Action"
 KEY_TEST="Test"
 KEY_OUTPUT="Output"
 #INPUT_REGEX_STR=r"\W*(?P<iterations>[0-9]+)[(\\t)\W]+(?P<timing>[0-9]+) ns\/op"
-INPUT_REGEX_STR = r"\s*(?P<iterations>\d+)[\\t ]+(?P<timing>\d+)\s+ns/op"
+INPUT_REGEX_STR = r'(?P<iterations>\d+)\s+(?P<timing>\d+(\.\d+)?)\sns'
 
 INPUT_REGEX=re.compile(INPUT_REGEX_STR)
+INPUT_SHORT=re.compile("[\n\t(\\t)\s]+")
 def parse(input_stream):
     sys.stdout.write("[")
     for line_idx,line in enumerate(input_stream.readlines()):
@@ -17,9 +18,8 @@ def parse(input_stream):
             continue
         if "Test" not in parsed_line or not parsed_line["Test"].startswith("Benchmark"):
             continue
-        reg_result=INPUT_REGEX.match(parsed_line[KEY_OUTPUT])
+        reg_result=INPUT_REGEX.search(parsed_line[KEY_OUTPUT])
         
-        #print(parsed_line[KEY_OUTPUT])
         if reg_result:
             group_dict=reg_result.groupdict()
             if line_idx>0:
@@ -27,7 +27,7 @@ def parse(input_stream):
             sys.stdout.write(json.dumps({
                     "name": f"go_{parsed_line["Package"]}.{parsed_line["Test"]}",
                     "unit": "ns",
-                    "value": int(group_dict["timing"]),
+                    "value": float(group_dict["timing"]),
                     "extra": f"Number of Iterations: {group_dict["iterations"]}\n"
                 }))
             sys.stdout.flush()

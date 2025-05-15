e0b072a68f94f21d9f609702035b02ef93b9c898
donmahallem <donmahallem@users.noreply.github.com>
2025-05-15 11:31:15 +0200

    test(go): add go24day08 benchmarks

--- go/aoc24/day08/part_2_test.go
+++ go/aoc24/day08/part_2_test.go
@@ -1,6 +1,7 @@
 package day08_test
 
 import (
+	"io"
 	"strings"
 	"testing"
 
@@ -11,5 +12,13 @@ func Test24Day08Part2(t *testing.T) {
 
 	if result := day08.Part2(strings.NewReader(testData)); result != 34 {
 		t.Errorf(`Expected %d to match %d`, result, 34)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	data := strings.NewReader(testData)
+	for b.Loop() {
+		data.Seek(0, io.SeekStart)
+		day08.Part2(data)
 	}
 }

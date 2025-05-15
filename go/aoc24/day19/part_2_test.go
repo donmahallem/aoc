c073c73dbe1d655af145240a84fbb8b955d7ac6c
ò_ó <donmahallem@users.noreply.github.com>
2025-05-16 00:12:17 +0200

    feat(go): several smaller improvements for go24day19 (#148)

--- go/aoc24/day19/part_2_test.go
+++ go/aoc24/day19/part_2_test.go
@@ -1,6 +1,7 @@
 package day19_test
 
 import (
+	"io"
 	"strings"
 	"testing"
 
@@ -15,7 +16,9 @@ func TestPart2(t *testing.T) {
 }
 
 func BenchmarkPart2(b *testing.B) {
+	data := strings.NewReader(testData)
 	for b.Loop() {
-		day19.Part2(strings.NewReader(testData))
+		data.Seek(0, io.SeekStart)
+		day19.Part2(data)
 	}
 }

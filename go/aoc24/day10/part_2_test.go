0fab0c8cf0fc81f1a4847e1205ea306cc3372ed6
donmahallem <donmahallem@users.noreply.github.com>
2025-05-15 14:07:35 +0200

    feat(go): several smaller improvements

--- go/aoc24/day10/part_2_test.go
+++ go/aoc24/day10/part_2_test.go
@@ -1,6 +1,7 @@
 package day10_test
 
 import (
+	"io"
 	"strings"
 	"testing"
 
@@ -14,7 +15,9 @@ func TestPart2(t *testing.T) {
 }
 
 func BenchmarkPart2(b *testing.B) {
+	testStream := strings.NewReader(testData)
 	for b.Loop() {
-		day10.Part2(strings.NewReader(testData))
+		testStream.Seek(0, io.SeekStart)
+		day10.Part2(testStream)
 	}
 }

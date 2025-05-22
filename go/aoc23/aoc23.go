b6af492ffdeb724dee73a143a20017e92ae46f60
ò_ó <donmahallem@users.noreply.github.com>
2025-05-22 19:03:28 +0200

    feat(go): add year23 day06 (#155)

--- go/aoc23/aoc23.go
+++ go/aoc23/aoc23.go
@@ -6,6 +6,7 @@ import (
 	"github.com/donmahallem/aoc/aoc23/day03"
 	"github.com/donmahallem/aoc/aoc23/day04"
 	"github.com/donmahallem/aoc/aoc23/day05"
+	"github.com/donmahallem/aoc/aoc23/day06"
 	"github.com/donmahallem/aoc/aoc_utils"
 )
 
@@ -16,4 +17,5 @@ func RegisterParts(registry *aoc_utils.Registry) {
 	regFunc(3, day03.Part1, day03.Part2)
 	regFunc(4, day04.Part1, day04.Part2)
 	regFunc(5, day05.Part1, day05.Part2)
+	regFunc(6, day06.Part1, day06.Part2)
 }

9fb726af58fadc7c43262f72b97c48376c77ed1e
ò_ó <donmahallem@users.noreply.github.com>
2025-05-22 12:31:20 +0200

    feat(go): improve year23 day05 performance (#154)
    * fix(go): improve go year23 day 05 performance
    
    * lint(go): apply suggestion

--- go/aoc23/day05/part_2_test.go
+++ go/aoc23/day05/part_2_test.go
@@ -14,6 +14,27 @@ func TestPart2(t *testing.T) {
 		t.Errorf(`Expected %d to be %d`, result, 46)
 	}
 }
+func TestIntersectInterval(t *testing.T) {
+	t.Run("Non insterescting", func(t *testing.T) {
+		a := day05.Interval{Start: 0, End: 100}
+		b := day05.Interval{Start: 100, End: 200}
+		result := day05.IntersectInterval(a, b)
+		if result != nil {
+			t.Errorf("Result should be nil")
+		}
+	})
+	t.Run("Non insterescting", func(t *testing.T) {
+		a := day05.Interval{Start: 0, End: 100}
+		b := day05.Interval{Start: 50, End: 200}
+		result := day05.IntersectInterval(a, b)
+		expected := day05.Interval{Start: 50, End: 100}
+		if result == nil {
+			t.Errorf("Result should not be nil")
+		} else if *result != expected {
+			t.Errorf("Result should not be %v. Not %v", expected, *result)
+		}
+	})
+}
 
 func BenchmarkPart2(b *testing.B) {
 	reader := strings.NewReader(testData)

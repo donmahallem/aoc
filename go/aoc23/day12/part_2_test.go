8ef072c3020da29232081f9f866130b7c985dc9f
donmahallem <donmahallem@users.noreply.github.com>
2025-10-19 23:05:30 +0200

    feat(go): add year23 day12

---
+++ go/aoc23/day12/part_2_test.go
@@ -0,0 +1,28 @@
+package day12_test
+
+import (
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/donmahallem/aoc/aoc23/day12"
+)
+
+func TestPart2(t *testing.T) {
+	t.Run("testData", func(t *testing.T) {
+		reader := strings.NewReader(testData)
+		result := day12.Part2(reader)
+		const expectedLen int = 525152
+		if result != expectedLen {
+			t.Errorf(`Expected %d galaxies to be parsed, got %d`, expectedLen, result)
+		}
+	})
+}
+
+func BenchmarkPart2(b *testing.B) {
+	reader := strings.NewReader(testData)
+	for b.Loop() {
+		reader.Seek(0, io.SeekStart)
+		day12.Part2(reader)
+	}
+}

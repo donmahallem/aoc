d993d943e9a8c0e4d8cca7986b05337542ab24c4
donmahallem <donmahallem@users.noreply.github.com>
2025-10-22 11:32:00 +0200

    feat(go): add year23 day12 part 1

---
+++ go/aoc23/day13/part_1.go
@@ -0,0 +1,92 @@
+package day13
+
+import (
+	"bufio"
+	"io"
+)
+
+type Block struct {
+	Rows []int
+	Cols []int
+}
+
+func ParseInput(r io.Reader) []Block {
+	scanner := bufio.NewScanner(r)
+
+	blocks := make([]Block, 0, 4)
+	block := Block{
+		Rows: make([]int, 0, 16),
+		Cols: nil,
+	}
+
+	currentHeight := 0
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		if len(line) == 0 {
+			currentHeight = 0
+			if len(block.Rows) > 0 {
+				blocks = append(blocks, block)
+			}
+			block = Block{
+				Rows: make([]int, 0, 16),
+				Cols: nil,
+			}
+			continue
+		}
+		if block.Cols == nil {
+			block.Cols = make([]int, len(line))
+		}
+		currentLine := 0
+		for idx, c := range line {
+
+			if c == '#' {
+				currentLine |= 1 << idx
+				block.Cols[idx] |= 1 << currentHeight
+			}
+		}
+		block.Rows = append(block.Rows, currentLine)
+		currentHeight++
+	}
+	if currentHeight > 0 {
+		blocks = append(blocks, block)
+	}
+	return blocks
+}
+
+func validateAxis(items []int, center int) bool {
+	for offset := 1; center-offset-1 >= 0 && center+offset < len(items); offset++ {
+		if items[center-offset-1] != items[center+offset] {
+			return false
+		}
+	}
+	return true
+}
+
+func findAxis(axisData []int) (int, bool) {
+	for centerIdx := 1; centerIdx < len(axisData); centerIdx++ {
+		if axisData[centerIdx-1] == axisData[centerIdx] {
+			if validateAxis(axisData, centerIdx) {
+				return centerIdx, true
+			}
+		}
+	}
+	return -1, false
+}
+func SolveBlock(b Block) int {
+	if rowAxis, ok := findAxis(b.Rows); ok {
+		return rowAxis * 100
+	}
+	if colAxis, ok := findAxis(b.Cols); ok {
+		return colAxis
+	}
+	panic("Could not find axis")
+}
+
+func Part1(in io.Reader) int {
+	start := ParseInput(in)
+	accum := 0
+	for _, block := range start {
+		accum += SolveBlock(block)
+	}
+	return accum
+}

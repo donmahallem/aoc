bf9b0b43a594151ed5ef513fde81dcea20337fae
ò_ó <donmahallem@users.noreply.github.com>
2025-10-23 09:24:45 +0200

    feat(go): add year23 day14 (#165)
    * feat(go): add year23 day14 part 1
    
    * feat(go): add year23 day14 part 2
    
    * feat(go): add year23 day14 part 2

---
+++ go/aoc23/day14/part_1_test.go
@@ -0,0 +1,64 @@
+package day14_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/donmahallem/aoc/aoc23/day14"
+)
+
+var testData string = `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`
+
+func TestParseInput(t *testing.T) {
+	t.Run("testData1", func(t *testing.T) {
+		const expected int = 10
+		reader := strings.NewReader(testData)
+		if res := day14.ParseInputPart1(reader); len(res) != expected {
+			t.Errorf(`Expected %d to be %d`, len(res), expected)
+		}
+
+	})
+	t.Run("test block 1", func(t *testing.T) {
+
+		reader := strings.NewReader(testData)
+		res := day14.ParseInputPart1(reader)
+		expectedRows := []uint{
+			5,
+			2,
+			4,
+			3,
+			0,
+			0,
+			3,
+			1,
+			0,
+			0,
+		}
+		for rowIdx := range expectedRows {
+			if res[rowIdx] != expectedRows[rowIdx] {
+				t.Errorf(`Expected row %d to be %d, got %d`, rowIdx, expectedRows[rowIdx], res[rowIdx])
+			}
+		}
+	})
+
+}
+
+func TestPart1(t *testing.T) {
+	t.Run("test block 2", func(t *testing.T) {
+
+		reader := strings.NewReader(testData)
+		res := day14.Part1(reader)
+		if res != 136 {
+			t.Errorf(`Expected number of blocks to be 136, got %d`, res)
+		}
+	})
+}
---
+++ go/aoc23/day14/part_2.go
@@ -0,0 +1,166 @@
+package day14
+
+import (
+	"bufio"
+	"hash/fnv"
+	"io"
+)
+
+type Field [][]Cell
+type Direction uint8
+
+const (
+	DirLeft  Direction = 'L'
+	DirRight Direction = 'R'
+	DirDown  Direction = 'D'
+	DirUp    Direction = 'U'
+)
+
+type Cell = byte
+
+const (
+	CellEmpty Cell = '.'
+	CellStone Cell = 'O'
+	CellWall  Cell = '#'
+)
+
+func ApplyGravity(field Field, dir Direction) {
+	switch dir {
+	case DirDown:
+		for colIdx := range field[0] {
+			lastEmptyIdx := len(field) - 1
+			for rowIdx := len(field) - 1; rowIdx >= 0; rowIdx-- {
+				switch field[rowIdx][colIdx] {
+				case CellStone:
+					if lastEmptyIdx != rowIdx {
+						field[lastEmptyIdx][colIdx] = CellStone
+						field[rowIdx][colIdx] = CellEmpty
+					}
+					lastEmptyIdx--
+				case CellWall:
+					lastEmptyIdx = rowIdx - 1
+				}
+			}
+		}
+	case DirUp:
+		for colIdx := range field[0] {
+			lastEmptyIdx := 0
+			for rowIdx := range len(field) {
+				switch field[rowIdx][colIdx] {
+				case CellStone:
+					if lastEmptyIdx != rowIdx {
+						field[lastEmptyIdx][colIdx] = CellStone
+						field[rowIdx][colIdx] = CellEmpty
+					}
+					lastEmptyIdx++
+				case CellWall:
+					lastEmptyIdx = rowIdx + 1
+				}
+
+			}
+		}
+	case DirLeft:
+		for rowIdx := range field {
+			lastEmptyIdx := 0
+			for colIdx := range len(field[0]) {
+				switch field[rowIdx][colIdx] {
+				case CellStone:
+					if lastEmptyIdx != colIdx {
+						field[rowIdx][lastEmptyIdx] = CellStone
+						field[rowIdx][colIdx] = CellEmpty
+					}
+					lastEmptyIdx++
+				case CellWall:
+					lastEmptyIdx = colIdx + 1
+				}
+			}
+		}
+	case DirRight:
+		for rowIdx := range field {
+			lastEmptyIdx := len(field[0]) - 1
+			for colIdx := len(field[0]) - 1; colIdx >= 0; colIdx-- {
+				switch field[rowIdx][colIdx] {
+				case CellStone:
+					if lastEmptyIdx != colIdx {
+						field[rowIdx][lastEmptyIdx] = CellStone
+						field[rowIdx][colIdx] = CellEmpty
+					}
+					lastEmptyIdx--
+				case CellWall:
+					lastEmptyIdx = colIdx - 1
+				}
+			}
+		}
+	}
+}
+
+func CycleDirections(field Field) {
+	ApplyGravity(field, DirUp)
+	ApplyGravity(field, DirLeft)
+	ApplyGravity(field, DirDown)
+	ApplyGravity(field, DirRight)
+}
+
+func ParseInputPart2(r io.Reader) Field {
+	scanner := bufio.NewScanner(r)
+
+	var field Field = make(Field, 0, 16)
+	for scanner.Scan() {
+		/**
+		 * Create a copy of the scanned bytes to avoid modifying the underlying buffer
+		 * on the next scan.
+		 */
+		row := append([]Cell(nil), scanner.Bytes()...)
+		field = append(field, row)
+	}
+	return field
+}
+
+func CalculateBeamLoad(field Field) uint {
+	var accum uint = 0
+	for rowIdx := range field {
+		for colIdx := range field[rowIdx] {
+			if field[rowIdx][colIdx] == CellStone {
+				accum += uint(len(field) - rowIdx)
+			}
+		}
+	}
+	return accum
+}
+
+func encodeField(field Field) uint64 {
+	fnvHasher := fnv.New64()
+	for rowIdx := range field {
+		fnvHasher.Write(field[rowIdx])
+	}
+	return fnvHasher.Sum64()
+}
+
+type CycleMemory = int
+
+const totalCycles CycleMemory = 1000000000
+
+func Part2(in io.Reader) uint {
+	field := ParseInputPart2(in)
+
+	memoryField := make(map[uint64]CycleMemory)
+	memoryScore := make(map[CycleMemory]uint)
+	/* Simulate subcycles and detect loops to skip ahead*/
+	for cycleIdx := CycleMemory(0); cycleIdx < totalCycles; cycleIdx++ {
+		CycleDirections(field)
+		encoded := encodeField(field)
+		if lastCycle, found := memoryField[encoded]; found {
+			skipCycles := totalCycles - cycleIdx - 1
+			cycleLength := cycleIdx - lastCycle
+			skips := skipCycles / cycleLength
+			cycleIdx += skips * cycleLength
+			remainingCycles := (totalCycles - cycleIdx - 1) % cycleLength
+			finalCycle := lastCycle + remainingCycles
+			return memoryScore[finalCycle]
+		} else {
+			memoryField[encoded] = cycleIdx
+			memoryScore[cycleIdx] = CalculateBeamLoad(field)
+		}
+	}
+	return 0
+}

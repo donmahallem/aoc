34856d310833b97c3529b8edd99113e15f76f9a5
ò_ó <donmahallem@users.noreply.github.com>
2025-11-05 09:46:18 +0100

    feat(go) year 24 day 04 improvements (#184)
    * feat(go): improve year 24 day 04
    
    * feat(go): improve year 24 day 04

--- go/aoc24/day04/part_1_test.go
+++ go/aoc24/day04/part_1_test.go
@@ -30,25 +30,6 @@ var testDataString = [][]byte{[]byte("MMMSXXMASM"),
 	[]byte("MAMMMXMMMM"),
 	[]byte("MXMXAXMASX")}
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
-func TestCheckBlock1(t *testing.T) {
-	result := day04.CheckBlock(testDataString)
-	if result != 18 {
-		t.Errorf(`Expected %d to match %d`, result, 18)
-	}
-}
-
-func TestCheckBlock2(t *testing.T) {
-	count := 0
-	for i := range len(testDataString) {
-		count += day04.CheckBlock(testDataString[max(0, i-3) : i+1])
-	}
-	if count != 30 {
-		t.Errorf(`Expected %d to match %d`, count, 30)
-	}
-}
-
 func Test24Day04Part1(t *testing.T) {
 
 	if result := day04.Part1(strings.NewReader(testData)); result != 18 {
--- go/aoc24/day04/part_2_test.go
+++ go/aoc24/day04/part_2_test.go
@@ -8,15 +8,6 @@ import (
 	"github.com/donmahallem/aoc/go/aoc24/day04"
 )
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
-func TestCheckMasBlock(t *testing.T) {
-	result := day04.CheckMasBlock(testDataString)
-	if result != 9 {
-		t.Errorf(`Expected %d to match %d`, result, 9)
-	}
-}
-
 func Test24Day04Part2(t *testing.T) {
 
 	if result := day04.Part2(strings.NewReader(testData)); result != 9 {

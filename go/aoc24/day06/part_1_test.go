23d002262223f383f1eb8e4df0c3e9014b43efae
donmahallem <donmahallem@users.noreply.github.com>
2025-11-05 16:02:27 +0100

    feat(go): improve year 24 day 06

--- go/aoc24/day06/part_1_test.go
+++ go/aoc24/day06/part_1_test.go
@@ -8,30 +8,6 @@ import (
 	"github.com/donmahallem/aoc/go/aoc24/day06"
 )
 
-func TestOutOfBoundsShouldBeInside(t *testing.T) {
-	x := int16(2)
-	y := int16(2)
-	testField := day06.NewField(10, 5, nil)
-	if day06.OutOfBounds(&testField, &x, &y) {
-		t.Errorf(`Expected %s to match`, "a")
-	}
-}
-
-func TestOutOfBoundsShouldBeOutside(t *testing.T) {
-	x := int16(-1)
-	y := int16(5)
-	testField := day06.NewField(6, 6, nil)
-	if !day06.OutOfBounds(&testField, &x, &y) {
-		t.Errorf(`Expected %s to match`, "a")
-	}
-	x = int16(1)
-	y = int16(-5)
-	testField = day06.NewField(6, 6, nil)
-	if !day06.OutOfBounds(&testField, &x, &y) {
-		t.Errorf(`Expected %s to match`, "a")
-	}
-}
-
 func TestPart1(t *testing.T) {
 	if res := day06.Part1(strings.NewReader(testData)); res != 41 {
 		t.Errorf(`Expected %d to match %d`, res, 41)
--- go/aoc24/day06/part_2_test.go
+++ go/aoc24/day06/part_2_test.go
@@ -19,14 +19,6 @@ var testData string = `....#.....
 #.........
 ......#...`
 
-func TestLoop(t *testing.T) {
-	testField, guard, _ := day06.ReadSource(strings.NewReader(testData))
-	testField.Field[8][1] = true
-	if !day06.TestLoop(&testField, guard) {
-		t.Errorf(`Expected to be valid`)
-	}
-}
-
 func TestPart2(t *testing.T) {
 	if res := day06.Part2(strings.NewReader(testData)); res != 6 {
 		t.Errorf(`Expected %d to match %d`, res, 6)

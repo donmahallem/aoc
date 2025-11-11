6d97edb87740c5ac73a8380366e8c3cd90040f4c
donmahallem <donmahallem@users.noreply.github.com>
2025-11-11 20:52:58 +0100

    feat(go): improve year 24 day 10

--- go/test_utils/test_files.go
+++ go/test_utils/test_files.go
@@ -2,9 +2,12 @@ package test_utils
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
+	"testing"
 )
 
 var (
@@ -31,5 +34,41 @@ func GetTestData(year int, day int) (string, bool) {
 		return "", false
 	} else {
 		return string(file), true
+	}
+}
+
+func TestFullDataForDate[A any](t *testing.T, year int, day int, fn func(in io.Reader) A) (A, bool) {
+	t.Helper() // define this function as a test helper
+	if CheckTestDataExists(year, day) {
+		data, ok := GetTestData(year, day)
+		if !ok {
+			t.Fatalf("Failed to read test data for %d day %d", year, day)
+		}
+		return fn(strings.NewReader(data)), true
+	} else {
+		t.Skipf("Test data for %d day %d not found, skipping...", year, day)
+	}
+	var zero A
+	return zero, false
+}
+
+/*
+Method for testing full data in benchmarks
+It will skip if the full test data file does not exist
+*/
+func BenchmarkFullDataForDate[A any](b *testing.B, year int, day int, fn func(in io.Reader) A) {
+	b.Helper() // define this function as a benchmark helper
+	if CheckTestDataExists(year, day) {
+		data, ok := GetTestData(year, day)
+		if !ok {
+			b.Fatalf("Failed to read test data for %d day %d", year, day)
+		}
+		reader := strings.NewReader(data)
+		for b.Loop() {
+			reader.Seek(0, io.SeekStart)
+			fn(reader)
+		}
+	} else {
+		b.Skipf("Test data for %d day %d not found, skipping...", year, day)
 	}
 }

9b17d8a77e25425a572c3cf7d2f87a66cf6b105e
ò_ó <donmahallem@users.noreply.github.com>
2025-11-11 22:04:56 +0100

    test(go): add year 23 day 22 test cases (#189)

--- go/aoc23/day22/part_1_test.go
+++ go/aoc23/day22/part_1_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/donmahallem/aoc/go/aoc23/day22"
+	"github.com/donmahallem/aoc/go/test_utils"
 )
 
 var testDataSample1 string = `1,0,1~1,2,1
@@ -51,13 +52,26 @@ func TestPart1(t *testing.T) {
 			t.Errorf(`Expected number of blocks to be %d, got %d`, expected, result)
 		}
 	})
+
+	t.Run("test real data", func(t *testing.T) {
+		result, ok := test_utils.TestFullDataForDate(t, 23, 22, day22.Part1)
+		if !ok || result != 503 {
+			t.Errorf(`Expected %d to be %d`, result, 503)
+		}
+	})
 }
 
 func BenchmarkPart1(b *testing.B) {
+	b.Run("benchmark sample data", func(b *testing.B) {
 
-	reader := strings.NewReader(testDataSample1)
-	for b.Loop() {
-		day22.Part1(reader)
-		reader.Seek(0, 0)
-	}
+		reader := strings.NewReader(testDataSample1)
+		for b.Loop() {
+			day22.Part1(reader)
+			reader.Seek(0, 0)
+		}
+	})
+
+	b.Run("benchmark full data", func(b *testing.B) {
+		test_utils.BenchmarkFullDataForDate(b, 23, 22, day22.Part1)
+	})
 }
--- go/aoc23/day22/part_2_test.go
+++ go/aoc23/day22/part_2_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/donmahallem/aoc/go/aoc23/day22"
+	"github.com/donmahallem/aoc/go/test_utils"
 )
 
 func TestPart2(t *testing.T) {
@@ -16,13 +17,25 @@ func TestPart2(t *testing.T) {
 			t.Errorf(`Expected number of blocks to be %d, got %d`, expected, result)
 		}
 	})
+
+	t.Run("test real data", func(t *testing.T) {
+		result, ok := test_utils.TestFullDataForDate(t, 23, 22, day22.Part2)
+		if !ok || result != 98431 {
+			t.Errorf(`Expected %d to be %d`, result, 98431)
+		}
+	})
 }
-
 func BenchmarkPart2(b *testing.B) {
+	b.Run("benchmark sample data", func(b *testing.B) {
 
-	reader := strings.NewReader(testDataSample1)
-	for b.Loop() {
-		day22.Part2(reader)
-		reader.Seek(0, 0)
-	}
+		reader := strings.NewReader(testDataSample1)
+		for b.Loop() {
+			day22.Part2(reader)
+			reader.Seek(0, 0)
+		}
+	})
+
+	b.Run("benchmark full data", func(b *testing.B) {
+		test_utils.BenchmarkFullDataForDate(b, 23, 22, day22.Part2)
+	})
 }

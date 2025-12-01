64e8d4cb583a962a6467617b2fa293a50053a675
ò_ó <donmahallem@users.noreply.github.com>
2025-12-01 19:58:06 +0100

    feat(python): add year25 day04

---
+++ python/aoc25/day04/test_part_1.py
@@ -0,0 +1,28 @@
+import unittest
+import io
+from aoc25.day03.part_1 import Part1
+
+testData = """..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."""
+
+
+class Test2025Day04Part01(unittest.TestCase):
+
+    def test_result(self):
+        with io.StringIO() as f:
+            f.write(testData)
+            f.seek(0)
+            result = Part1(f)
+            self.assertEqual(result, 13, "The sum is wrong.")
+
+
+if __name__ == "__main__":
+    unittest.main()

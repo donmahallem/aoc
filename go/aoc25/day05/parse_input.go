6f473f035ac10e2325874ad18195cc119070162f
donmahallem <donmahallem@users.noreply.github.com>
2025-12-05 10:08:15 +0100

    feat(go): add year25 day05

---
+++ go/aoc25/day05/parse_input.go
@@ -0,0 +1,51 @@
+package day05
+
+import (
+	"bufio"
+	"io"
+
+	"github.com/donmahallem/aoc/go/aoc_utils/math"
+)
+
+type validRange = math.Interval[uint64]
+
+type taskData struct {
+	validRanges []validRange
+	ingredients []uint64
+}
+
+func parseInput(in io.Reader) taskData {
+	scanner := bufio.NewScanner(in)
+	var data taskData
+	inRangesBlock := true
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		if len(line) == 0 {
+			inRangesBlock = false
+			continue
+		}
+		if inRangesBlock {
+			currentInterval := validRange{}
+			currentNumber := &currentInterval.Min
+			for i := range len(line) {
+				b := line[i]
+				if b >= '0' && b <= '9' {
+					*currentNumber = *currentNumber*10 + uint64(b-'0')
+				} else if b == '-' {
+					currentNumber = &currentInterval.Max
+				}
+			}
+			data.validRanges = append(data.validRanges, currentInterval)
+		} else {
+			currentNumber := uint64(0)
+			for i := range len(line) {
+				b := line[i]
+				if b >= '0' && b <= '9' {
+					currentNumber = currentNumber*10 + uint64(b-'0')
+				}
+			}
+			data.ingredients = append(data.ingredients, currentNumber)
+		}
+	}
+	return data
+}
---
+++ go/aoc25/day05/parse_input_test.go
@@ -0,0 +1,51 @@
+package day05
+
+import (
+	_ "embed"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/donmahallem/aoc/go/test_utils"
+)
+
+//go:embed sample.txt
+var testData string
+
+func Test_parseInput(t *testing.T) {
+	tests := taskData{
+		validRanges: []validRange{
+			{Min: 3, Max: 5},
+			{Min: 10, Max: 14},
+			{Min: 16, Max: 20},
+			{Min: 12, Max: 18},
+		},
+		ingredients: []uint64{1, 5, 8, 11, 17, 32},
+	}
+	t.Run("test parseInput", func(t *testing.T) {
+		reader := strings.NewReader(testData)
+		parsedData := parseInput(reader)
+		if !reflect.DeepEqual(parsedData.validRanges, tests.validRanges) {
+			t.Errorf("Expected validRanges to be %+v, got %+v", tests.validRanges, parsedData.validRanges)
+		}
+		if !reflect.DeepEqual(parsedData.ingredients, tests.ingredients) {
+			t.Errorf("Expected ingredients to be %+v, got %+v", tests.ingredients, parsedData.ingredients)
+		}
+	})
+}
+
+func BenchmarkXxx(b *testing.B) {
+
+	b.Run("benchmark sample data", func(b *testing.B) {
+
+		reader := strings.NewReader(testData)
+		for b.Loop() {
+			parseInput(reader)
+			reader.Seek(0, 0)
+		}
+	})
+
+	b.Run("benchmark full data", func(b *testing.B) {
+		test_utils.BenchmarkFullDataForDate(b, 25, 5, parseInput)
+	})
+}

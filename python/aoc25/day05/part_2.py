97ce672ed9414841031a1de203616bd1587f8d94
donmahallem <donmahallem@users.noreply.github.com>
2025-12-05 07:11:49 +0100

    style(python): yapf is yapping

--- python/aoc25/day05/part_2.py
+++ python/aoc25/day05/part_2.py
@@ -5,11 +5,11 @@ from aoc25.day05.part_1 import parseInput, compressRanges
 
 
 def Part2(input: typing.TextIO) -> int:
-    ranges,_ = parseInput(input)
-    compresed= compressRanges(ranges)
-    goodIngredients=0
-    for rL,rR in compresed:
-        goodIngredients+= rR-rL+1
+    ranges, _ = parseInput(input)
+    compresed = compressRanges(ranges)
+    goodIngredients = 0
+    for rL, rR in compresed:
+        goodIngredients += rR - rL + 1
     return goodIngredients
 
 

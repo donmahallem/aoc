e4d775b726642dae729277050dc6243da78d21b2
donmahallem <donmahallem@users.noreply.github.com>
2025-12-06 22:31:23 +0100

    feat(python): add year25 day02

---
+++ python/aoc25/day02/parse_input.py
@@ -0,0 +1,13 @@
+import codecs
+import sys
+import typing
+
+
+# parseInput Gen
+def parseInputGen(inp: typing.TextIO) -> typing.Generator[int, None, None]:
+    for line in inp:
+        for pair in line.strip().split(','):
+            if pair == '':
+                continue
+            a, b = pair.split('-')
+            yield (int(a), int(b))
---
+++ python/aoc25/day02/part_1.py
@@ -0,0 +1,20 @@
+import sys
+import typing
+from .parse_input import parseInputGen
+from .find_repeated_blocks import find_repeated_blocks
+
+
+def Part1(input: typing.TextIO) -> int:
+    total_sum = 0
+    for start, end in parseInputGen(input):
+        invalids = find_repeated_blocks(start, end)
+        for num, k in invalids.items():
+            if k == 2:
+                total_sum += num
+    return total_sum
+
+
+
+
+if __name__ == "__main__":
+    print("Part 1:", Part1(sys.stdin))

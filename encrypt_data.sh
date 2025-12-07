67cbc7373c4082e7c4659ae410a74175315a5db7
donmahallem <donmahallem@users.noreply.github.com>
2025-12-07 15:49:00 +0100

    ci(github): update test data

--- encrypt_data.sh
+++ encrypt_data.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-tar -cvzf - ./data | gpg  --batch --yes --pinentry-mode loopback --passphrase "$DATA_PW" --symmetric > data.tar.gz.gpg 
+
+if [ -z "$DATA_PW" ]; then
+  echo "Error: DATA_PW environment variable is not set."
+  exit 1
+fi
+
+# Use --batch --yes to avoid interactive prompts
+# Use --passphrase-fd 0 to read password from stdin (safer) or keep using --passphrase
+tar -cvzf - ./data | gpg --batch --yes --pinentry-mode loopback --passphrase "$DATA_PW" --symmetric --cipher-algo AES256 > data.tar.gz.gpg

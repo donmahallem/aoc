9803a26fb0c5f2a2a3cb95ab44bb5f09593378aa
donmahallem <donmahallem@users.noreply.github.com>
2025-12-08 07:42:58 +0100

    feat(go): add go wasi stuff

---
+++ go/solver.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"time"
+
+	"github.com/donmahallem/aoc/go/aoc23"
+	"github.com/donmahallem/aoc/go/aoc24"
+	"github.com/donmahallem/aoc/go/aoc25"
+	"github.com/donmahallem/aoc/go/aoc_utils"
+)
+
+type SolveResult struct {
+	Result   string
+	Duration time.Duration
+	Error    error
+}
+
+func RunSolver(year, day, part int, input io.Reader) SolveResult {
+	partSelector := aoc_utils.PartSelector{
+		Year: year,
+		Day:  day,
+		Part: part,
+	}
+
+	partRegistry := aoc_utils.NewRegistry()
+	aoc23.RegisterParts(&partRegistry)
+	aoc24.RegisterParts(&partRegistry)
+	aoc25.RegisterParts(&partRegistry)
+
+	takeFun, ok := partRegistry.GetPart(partSelector)
+	if !ok {
+		return SolveResult{Error: fmt.Errorf("could not find requested part %v", partSelector)}
+	}
+
+	function := reflect.ValueOf(takeFun)
+	startTime := time.Now()
+	results := function.Call([]reflect.Value{reflect.ValueOf(input)})
+	endTime := time.Now()
+
+	res := results[0].Interface()
+	var resStr string
+
+	switch v := res.(type) {
+	case int, uint, int32, uint32, uint16, uint8, uint64, int16, int8, int64:
+		resStr = fmt.Sprintf("%d", v)
+	case string:
+		resStr = v
+	case []int:
+		for i, val := range v {
+			if i > 0 {
+				resStr += ","
+			}
+			resStr += fmt.Sprintf("%d", val)
+		}
+	case aoc_utils.Point[int16]:
+		resStr = fmt.Sprintf("{X:%d,Y:%d}", v.X, v.Y)
+	default:
+		resStr = fmt.Sprintf("%v", v)
+	}
+
+	return SolveResult{
+		Result:   resStr,
+		Duration: endTime.Sub(startTime),
+	}
+}

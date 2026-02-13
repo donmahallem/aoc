738eabd4a9a3ad5a8151c233be35841f52ac163d
donmahallem <donmahallem@users.noreply.github.com>
2026-02-13 10:27:21 +0100

    feat(cpp): add aoc24 day 03

--- cpp/src/aoc24/aoc24.cpp
+++ cpp/src/aoc24/aoc24.cpp
@@ -7,5 +7,8 @@ namespace Aoc24
         auto regFunc = registry->CreateYearRegistry(24);
         regFunc(1, Aoc24Day01::Part1, Aoc24Day01::Part2);
         regFunc(2, Aoc24Day02::Part1, Aoc24Day02::Part2);
+        regFunc(3, Aoc24Day03::Part1, Aoc24Day03::Part2);
+        regFunc(4, Aoc24Day04::Part1, Aoc24Day04::Part2);
+        regFunc(5, Aoc24Day05::Part1, Aoc24Day05::Part2);
     }
 }

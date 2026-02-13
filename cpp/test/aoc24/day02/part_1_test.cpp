5a2cab5d405811cda60a272fbea151b2dc3dd61f
ò_ó <donmahallem@users.noreply.github.com>
2026-02-13 13:45:28 +0100

    test(cpp): remove redundant tests (#243)
    * test(cpp): remove redundant tests
    
    * chore(cpp): update README info

--- cpp/test/aoc24/day02/part_1_test.cpp
+++ cpp/test/aoc24/day02/part_1_test.cpp
@@ -10,17 +10,6 @@ namespace AocTest
         namespace
         {
 
-            TEST(Day02Part1Test, Part1)
-            {
-                std::istringstream dummyStream("7 6 4 2 1\n"
-                                               "1 2 7 8 9\n"
-                                               "9 7 6 2 1\n"
-                                               "1 3 2 4 5\n"
-                                               "8 6 4 4 1\n"
-                                               "1 3 6 7 9");
-                int result = Aoc24Day02::Part1(dummyStream);
-                EXPECT_EQ(result, 2);
-            }
             TEST(checklineTest, checkPasses)
             {
                 std::vector<int> testData{7, 6, 4, 2, 1};

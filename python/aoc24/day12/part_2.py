63726585bfbb77e438d60a81b6c70c6c48af50a6
donmahallem <donmahallem@users.noreply.github.com>
2026-02-26 21:25:13 +0100

    style(pyton): yapf is yapping

--- python/aoc24/day12/part_2.py
+++ python/aoc24/day12/part_2.py
@@ -1,5 +1,4 @@
 import typing
-import numpy as np
 
 from .shared import _Field
 
--- python/aoc24/day12/shared.py
+++ python/aoc24/day12/shared.py
@@ -1,7 +1,8 @@
 import typing
 
+_coord: typing.TypeAlias = tuple[int, int]
 
-type _coord=tuple[int,int]
+
 class _Field:
     # Corrected to plural: __slots__
     __slots__ = ["width", "height", "data"]
@@ -11,24 +12,26 @@ class _Field:
         self.height = height
         self.data = data if data is not None else []
 
-    @classmethod  
+    @classmethod
     def parse_input(cls, file_stream: typing.TextIO) -> "_Field":
         data = []
         width = 0
-        
+
         for line in file_stream:
             clean_line = line.strip()
-            
+
             if not clean_line:
-                if data: # terminate parsing after hitting empty line after content
+                if data:  # terminate parsing after hitting empty line after content
                     break
-                continue 
+                continue
 
             current_width = len(clean_line)
             if width == 0:
                 width = current_width
             elif current_width != width:
-                raise ValueError(f"Irregular line length: expected {width}, got {current_width}")
+                raise ValueError(
+                    f"Irregular line length: expected {width}, got {current_width}"
+                )
 
             data.append(bytearray(clean_line, 'ascii'))
 
@@ -37,64 +40,61 @@ class _Field:
     def get(self, x: int, y: int) -> int:
         return self.data[y][x]
 
-    def collect_islands(self)-> list[set[_coord]]:
-        islands:list[set[_coord]]=list()
-        dirs =[(0,1),(1,0),(0,-1),(-1,0)]
-        visited : set[_coord]=set()
+    def collect_islands(self) -> list[set[_coord]]:
+        islands: list[set[_coord]] = list()
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        visited: set[_coord] = set()
         for x in range(self.width):
             for y in range(self.height):
-                start = (y,x)
+                start = (y, x)
                 if start in visited:
                     continue
                 # flood fill cells into buffer
-                buffer: set[_coord]=set()
+                buffer: set[_coord] = set()
                 toVisit = [start]
-                groupValue= self.data[y][x]
-                while len(toVisit)>0:
+                groupValue = self.data[y][x]
+                while len(toVisit) > 0:
                     curPos = toVisit.pop()
                     visited.add(curPos)
                     buffer.add(curPos)
-                    curY,curX=curPos
-                    for dirY,dirX in dirs:
-                        nextY,nextX = curY+dirY,curX+dirX
+                    curY, curX = curPos
+                    for dirY, dirX in dirs:
+                        nextY, nextX = curY + dirY, curX + dirX
                         # out of bounds checks
-                        if nextY<0 or nextX<0 or nextY>=self.height or nextX>=self.width:
+                        if nextY < 0 or nextX < 0 or nextY >= self.height or nextX >= self.width:
                             continue
-                        nextPos=(nextY,nextX)
+                        nextPos = (nextY, nextX)
                         if nextPos in visited:
                             continue
-                        if self.data[nextY][nextX]==groupValue and nextPos not in buffer:
+                        if self.data[nextY][
+                                nextX] == groupValue and nextPos not in buffer:
                             toVisit.append(nextPos)
                 islands.append(buffer)
         return islands
 
     @staticmethod
-    def count_edges(island:list[_coord],straight_edges:bool = False):
+    def count_edges(island: list[_coord], straight_edges: bool = False):
         '''
         Counts the number of edges of the island
         '''
-        
+
         steps = 0
-        dirChanges =0
+        dirChanges = 0
 
-        edgeChecks = [
-            (0,1),(1,1),
-            (1,0),(1,-1),
-            (0,-1),(-1,-1),
-            (-1,0),(-1,1)
-        ]
-        for y,x in island:
-            for i in range(0,8,2):
-                cellA = (y+edgeChecks[i][0],x+edgeChecks[i][1])
-                cellB = (y+edgeChecks[(i+2)%8][0],x+edgeChecks[(i+2)%8][1])
+        edgeChecks = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1),
+                      (-1, 0), (-1, 1)]
+        for y, x in island:
+            for i in range(0, 8, 2):
+                cellA = (y + edgeChecks[i][0], x + edgeChecks[i][1])
+                cellB = (y + edgeChecks[(i + 2) % 8][0],
+                         x + edgeChecks[(i + 2) % 8][1])
                 if cellA not in island:
-                    steps+=1
+                    steps += 1
                     if cellB not in island:
-                        dirChanges+=1
+                        dirChanges += 1
                 elif cellB in island:
-                    cellC = (y+edgeChecks[(i+1)%8][0],x+edgeChecks[(i+1)%8][1])
+                    cellC = (y + edgeChecks[(i + 1) % 8][0],
+                             x + edgeChecks[(i + 1) % 8][1])
                     if cellC not in island:
-                        dirChanges+=1
-        return steps,dirChanges
-
-    
+                        dirChanges += 1
+        return steps, dirChanges

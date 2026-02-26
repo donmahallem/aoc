50b24283b333012e0f3682aa2423ee2ee9d2f57c
donmahallem <donmahallem@users.noreply.github.com>
2026-02-26 21:07:50 +0100

    style(python): fix year 24 day 12 styling

--- python/aoc24/day12/shared.py
+++ python/aoc24/day12/shared.py
@@ -7,14 +7,14 @@ class _Field:
     # Corrected to plural: __slots__
     __slots__ = ["width", "height", "data"]
 
-    def __init__(self, width: int = 0, height: int = 0, data: list = None):
+    def __init__(self, width: int = 0, height: int = 0, data: list[bytearray]|None = None):
         self.width = width
         self.height = height
         self.data = data if data is not None else []
 
     @classmethod
     def parse_input(cls, file_stream: typing.TextIO) -> "_Field":
-        data = []
+        data: list[bytearray] = []
         width = 0
 
         for line in file_stream:

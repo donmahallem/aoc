1a11b0b86510bafcfa85298b2116d862d10359e3
donmahallem <donmahallem@users.noreply.github.com>
2026-02-26 14:55:31 +0100

    perf(python): improve python year 24 day 12

--- python/aoc24/day12/part_2.py
+++ python/aoc24/day12/part_2.py
@@ -1,48 +1,14 @@
 import typing
 import numpy as np
-from .part_1 import parseInput, find_connected
 
-
-def count_straight_edges(field, coords):
-    test = np.zeros((field.shape[0] + 2, field.shape[1] + 2), dtype=np.int8)
-    for y, x in coords:
-        test[y + 1, x + 1] = 1
-    diff_0 = np.diff(test, axis=0)
-    edges = 0
-    for y in range(diff_0.shape[0]):
-        last_val = 0
-        for x in range(diff_0.shape[1]):
-            if diff_0[y, x] != 0 and (last_val != diff_0[y, x]):
-                edges += 1
-                last_val = diff_0[y, x]
-            elif diff_0[y, x] == 0 and last_val != 0:
-                last_val = 0
-    # horizontale Kanten
-    diff_1 = np.diff(test, axis=1)
-    for x in range(diff_1.shape[1]):
-        last_val = 0
-        for y in range(diff_1.shape[0]):
-            if diff_1[y, x] != 0 and (last_val != diff_1[y, x]):
-                edges += 1
-                last_val = diff_1[y, x]
-            elif diff_1[y, x] == 0 and last_val != 0:
-                last_val = 0
-    return edges
+from .shared import _Field
 
 
 def Part2(input: typing.TextIO) -> int:
-    field = parseInput(input)
-    checked_map = np.zeros((field.shape), dtype=np.uint8)
-    groups = list()
-    for y in range(field.shape[0]):
-        for x in range(field.shape[1]):
-            if checked_map[y, x] == 1:
-                continue
-            group = set({(y, x)})
-            find_connected(field, checked_map, y, x, field[y, x], group)
-            key = chr(field[y, x])
-            groups.append((key, group))
-    count = 0
-    for key, group in groups:
-        count += count_straight_edges(field, group) * len(group)
-    return count
+    field = _Field.parse_input(input)
+    islands = field.collect_islands()
+    total = 0
+    for island in islands:
+        _, edges = _Field.count_edges(island)
+        total += len(island) * edges
+    return total
---
+++ python/aoc24/day12/test_shared.py
@@ -0,0 +1,47 @@
+import io
+import unittest
+from .shared import _Field
+
+class AocYear24Day12_Field(unittest.TestCase):
+    
+    def test_constructor(self):
+        f = _Field(1,2,[[1,2]])
+        self.assertEqual(f.width,1)
+        self.assertEqual(f.height,2)
+        self.assertEqual(f.data,[[1,2]])
+
+    def test_parseInput_simple(self):
+        inp = '''AAA\nBBB\nCCB'''
+        f_obj = io.StringIO(inp)
+        f = _Field.parse_input(f_obj)
+        self.assertEqual(f.width,3)
+        self.assertEqual(f.height,3)
+        self.assertEqual(f.data,[bytearray(b'AAA'), bytearray(b'BBB'), bytearray(b'CCB')])
+
+    def test_collectIslands_simple(self):
+        inp = '''AAA\nBBB\nCCB'''
+        f_obj = io.StringIO(inp)
+        f = _Field.parse_input(f_obj)
+        islands = f.collect_islands()
+        self.assertEqual(len(islands),3,f"Expected 3 islands, got {len(islands)}\n{islands}")
+        self.assertEqual(islands[0],{(0,0),(0,1),(0,2)},f"Expected first island to be {(0,0),(0,1),(0,2)}, got {islands[0]}")
+        self.assertEqual(islands[1],{(1,0),(1,1),(1,2),(2,2)})
+    def test_collectIslands_edges(self):
+        inp = '''ABA\nBAB'''
+        f_obj = io.StringIO(inp)
+        f = _Field.parse_input(f_obj)
+        islands = f.collect_islands()
+        self.assertEqual(len(islands),6,f"Expected 6 islands, got {len(islands)}\n{islands}")
+
+    def test_count_edges_L_shape(self):
+        # L shape
+        test_island = set([(0,0),(1,0),(1,1)])
+        edges = _Field.count_edges(test_island)
+        self.assertEqual(edges,(8,6))
+    def test_count_edges_plus(self):
+        # Plus shape
+        test_island = set([(0,1),(1,0),(1,1),(1,2),(2,1)])
+        edges = _Field.count_edges(test_island)
+        self.assertEqual(edges,(12,12))
+if __name__ == '__main__':
+    unittest.main()

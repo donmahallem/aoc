12f6693d98b283b9c0b56e9c001987945f041139
ò_ó <donmahallem@users.noreply.github.com>
2026-03-02 20:48:20 +0100

    feat(python): slightly improve year 24 day03 (#294)

--- python/aoc24/day03/part_1.py
+++ python/aoc24/day03/part_1.py
@@ -4,10 +4,10 @@ import typing
 
 
 def Part1(input: typing.TextIO) -> int:
-    data = "".join(input.readlines())
-    comp = re.compile(r"mul\((\d+)\,(\d+)\)", flags=re.MULTILINE)
-    findings: list[tuple[str, str]] = comp.findall(data)
-    return sum([int(a) * int(b) for a, b in findings])
+    data = input.read()
+    pattern = re.compile(r"mul\((\d+),(\d+)\)")
+    findings = pattern.findall(data)
+    return sum(int(a) * int(b) for a, b in findings)
 
 
 if __name__ == "__main__":

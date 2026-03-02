e628de4bf233568e7f38f1b8640d4976b144e76e
donmahallem <donmahallem@users.noreply.github.com>
2026-03-02 14:49:29 +0100

    feat(python): add year 23 day 02

---
+++ python/aoc23/day02/part_1.py
@@ -0,0 +1,19 @@
+import sys
+import typing
+from .shared import parse_line
+
+
+def Part1(input: typing.TextIO) -> int:
+    total = 0
+    for line in input:
+        line = line.strip()
+        if not line:
+            continue
+        game_id, blocks = parse_line(line)
+        if all(b.red <= 12 and b.green <= 13 and b.blue <= 14 for b in blocks):
+            total += game_id
+    return total
+
+
+if __name__ == "__main__":
+    print(Part1(sys.stdin))

1645a134af9cde344de695528243c33e9b874ffa
ò_ó <donmahallem@users.noreply.github.com>
2026-03-04 19:26:07 +0100

    feat(python): remove numpy year 24 day 14 (#304)

--- python/aoc24/day14/part_1.py
+++ python/aoc24/day14/part_1.py
@@ -1,39 +1,35 @@
 import typing
-import re
-import numpy as np
-
-
-def parseInput(input: typing.TextIO) -> tuple[np.ndarray, int]:
-    data = [line.strip() for line in input.readlines()]
-    parse_regex = re.compile(r"p=([+-]?\d+),([+-]?\d+).+?([+-]?\d+),([+-]?\d+)")
-    robots = np.zeros((len(data), 4), dtype=np.int64)
-    for i, machine in enumerate(data):
-        reg_res = parse_regex.match(machine)
-        if reg_res:
-            robots[i, 0] = int(reg_res.groups()[0])
-            robots[i, 1] = int(reg_res.groups()[1])
-            robots[i, 2] = int(reg_res.groups()[2])
-            robots[i, 3] = int(reg_res.groups()[3])
-    return robots, len(data)
+from .shared import _parse_input
 
 
 def Part1(
     input: typing.TextIO, width: int = 101, height: int = 103, steps: int = 100
 ) -> int:
-    robots, lines = parseInput(input)
+    pxs, pys, vxs, vys = _parse_input(input)
 
     # Heuristic: small samples use 11x7 grid (per AoC example).
-    if lines <= 20:
+    if len(pxs) <= 20:
         width = 11
         height = 7
-    step = robots[:, 0:2] + robots[:, 2:4] * steps
-    step[:, 0] = step[:, 0] % width
-    step[:, 1] = step[:, 1] % height
 
     center_x = width // 2
     center_y = height // 2
-    q1 = (step[:, 0] < center_x) * (step[:, 1] < center_y)
-    q2 = (step[:, 0] > center_x) * (step[:, 1] < center_y)
-    q3 = (step[:, 0] > center_x) * (step[:, 1] > center_y)
-    q4 = (step[:, 0] < center_x) * (step[:, 1] > center_y)
-    return sum(q1) * sum(q2) * sum(q3) * sum(q4)
+
+    q1 = q2 = q3 = q4 = 0
+
+    for px, py, vx, vy in zip(pxs, pys, vxs, vys):
+        nx = (px + vx * steps) % width
+        ny = (py + vy * steps) % height
+
+        if nx < center_x:
+            if ny < center_y:
+                q1 += 1
+            elif ny > center_y:
+                q4 += 1
+        elif nx > center_x:
+            if ny < center_y:
+                q2 += 1
+            elif ny > center_y:
+                q3 += 1
+
+    return q1 * q2 * q3 * q4

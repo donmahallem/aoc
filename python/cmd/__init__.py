e7e21bdd2190e4dc1cf9bf33da493f8ab6232ded
donmahallem <donmahallem@users.noreply.github.com>
2026-03-05 07:41:56 +0100

    feat(python): setup pytest benchmark

--- python/cmd/__init__.py
+++
@@ -1,6 +0,0 @@
-from .cmd_benchmark import BenchmarkArgs, BenchmarkResult
-from .cmd_list import ListArgs, ListResult
-from .cmd_parser import SolveArgs, SolverResult
-from .collect_solvers import collect_solvers
-from .const import CommonArgs, SUPPORTED_YEARS, SUPPORTED_DAYS, SUPPORTED_PARTS, Solver
-from .cli_output import CliOutput

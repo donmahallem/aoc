d835d67a3f7316ebd9d7243ae7a14335a82dad49
ò_ó <donmahallem@users.noreply.github.com>
2026-03-06 19:54:29 +0100

    perf(python): very slightly improve year 24 day 03 (#316)

--- python/aoc24/day03/part_1.py
+++ python/aoc24/day03/part_1.py
@@ -5,9 +5,8 @@ import typing
 
 def Part1(input: typing.TextIO) -> int:
     data = input.read()
-    pattern = re.compile(r"mul\((\d+),(\d+)\)")
-    findings = pattern.findall(data)
-    return sum(int(a) * int(b) for a, b in findings)
+    pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+    return sum([int(a) * int(b) for a, b in pattern.findall(data)])
 
 
 if __name__ == "__main__":

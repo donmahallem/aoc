d6a51ffcc215d1b584cda64b43474f4d02abb2ef
donmahallem <donmahallem@users.noreply.github.com>
2026-03-07 12:11:40 +0100

    perf(python): improve year 25 day 15 part 1 performance

--- python/aoc24/day15/shared.py
+++ python/aoc24/day15/shared.py
@@ -1,8 +1,45 @@
+from util.typed_coord import Coord, CoordX, CoordY
 import typing
-import numpy as np
 
 
-def translateMovement(a):
+def _parse_field(
+    input_data: typing.TextIO,
+) -> tuple[set[Coord], set[Coord], Coord, list[Coord]]:
+    walls: set[Coord] = set()
+    player_position: Coord | None = None
+    boxes: set[Coord] = set()
+    currentRow = 0
+    for line in input_data:
+        line = line.strip()
+        if line == "":
+            break
+        for col in range(0, len(line)):
+            match line[col]:
+                case "#":
+                    walls.add(Coord((CoordX(currentRow), CoordY(col))))
+                case "O":
+                    boxes.add(Coord((CoordX(currentRow), CoordY(col))))
+                case "@":
+                    player_position = Coord((CoordX(currentRow), CoordY(col)))
+        currentRow += 1
+    movements: list[Coord] = list()
+    for line in input_data:
+        line = line.strip()
+        if line == "":
+            break
+        for dir in line:
+            match dir:
+                case "<":
+                    movements.append(Coord((CoordX(0), CoordY(-1))))
+                case ">":
+                    movements.append(Coord((CoordX(0), CoordY(1))))
+                case "v":
+                    movements.append(Coord((CoordX(1), CoordY(0))))
+                case "^":
+                    movements.append(Coord((CoordX(-1), CoordY(0))))
+    return walls, boxes, player_position, movements
+
+def translateMovement(a) -> Coord:
     if a == "<":
         return (0, -1)
     elif a == ">":

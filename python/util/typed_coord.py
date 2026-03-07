162cb9a9e2fe8e6e50f1e8973c30bb52aeb6d4a3
donmahallem <donmahallem@users.noreply.github.com>
2026-03-07 12:23:06 +0100

    style(python): add type description

--- python/util/typed_coord.py
+++ python/util/typed_coord.py
@@ -1,6 +1,9 @@
 from typing import NewType
 
+# Designating coord X
 CoordX = NewType("CoordX", int)
+# Designating coord Y
 CoordY = NewType("CoordY", int)
 
+# Designating a coordinate as a tuple of (Y, X)
 Coord = NewType("Coord", tuple[CoordY, CoordX])

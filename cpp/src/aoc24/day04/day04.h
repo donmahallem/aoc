1d2ae140403bea70d717335d0ab142fb1453043d
ò_ó <donmahallem@users.noreply.github.com>
2026-03-08 23:11:09 +0100

    feat(cpp): add year 24 day 04 (#337)

---
+++ cpp/src/aoc24/day04/day04.h
@@ -0,0 +1,14 @@
+#ifndef AOC24_DAY04_H
+#define AOC24_DAY04_H
+
+#include <istream>
+#include <string>
+
+namespace aoc24::day04
+{
+    inline constexpr std::size_t BUFFER_SIZE = 512;
+    long long Part1(std::istream& in);
+    long long Part2(std::istream& in);
+}  // namespace aoc24::day04
+
+#endif  // AOC24_DAY04_H
